525e67d72d2fbd4a7bae12583548853db55ed061
Jimmy Mathews <mathewj2@mskcc.org>
2021-06-08 12:41:47 -0400

    trying to shorten names

--- docs/conf.py
+++ docs/conf.py
@@ -184,4 +184,6 @@ epub_exclude_files = ['search.html']
 # -- Options for todo extension ----------------------------------------------
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = True
+todo_include_todos = False
+
+add_module_names = False

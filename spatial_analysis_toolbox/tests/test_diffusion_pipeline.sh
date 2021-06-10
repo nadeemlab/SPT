92fdf0c864465c323156a30d5c91f9e999f43307
Jimmy Mathews <mathewj2@mskcc.org>
2021-06-10 19:37:08 -0400

    add cleanup to tests

--- spatial_analysis_toolbox/tests/test_diffusion_pipeline.sh
+++ spatial_analysis_toolbox/tests/test_diffusion_pipeline.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export DEBUG=1
+rm -rf output/
 cp sample_config_file_diffusion.txt .sat_pipeline.config
 sat-pipeline
 rm .sat_pipeline.config
--- spatial_analysis_toolbox/tests/test_proximity_pipeline.sh
+++ spatial_analysis_toolbox/tests/test_proximity_pipeline.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export DEBUG=1
+rm -rf output/
 cp sample_config_file_proximity.txt .sat_pipeline.config
 sat-pipeline
 rm .sat_pipeline.config

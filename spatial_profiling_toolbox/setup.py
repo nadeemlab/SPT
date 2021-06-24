fd25092c509b12f926cffc0c389caa478ef0d2b6
Jimmy Mathews <mathewj2@mskcc.org>
2021-06-24 14:48:52 -0400

    updating version to 0.4.0, reflects less automatic pipeline interim design choice

--- spatial_profiling_toolbox/setup.py
+++ spatial_profiling_toolbox/setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(dir, 'requirements.txt'), 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='spatial-profiling-toolbox',
-    version='0.3.0',
+    version='0.4.0',
     author='Rami Vanguri, James Mathews',
     author_email='vangurir@mskcc.org',
     description='Toolbox for spatial analysis of pathology imaging.',

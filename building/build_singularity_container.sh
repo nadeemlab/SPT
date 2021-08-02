7011ee84d95245547bc674b35b461b49f641628b
James Mathews <mail@jmath.read-books.org>
2021-08-02 18:01:15 -0400

    fix reference to version txt

--- building/build_singularity_container.sh
+++ building/build_singularity_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 datestring=$(date +'%m-%d-%Y_%H-%M')
-version=$(cat ../spatial_profiling_toolbox/spatial_profiling_toolbox/version.txt)
+version=$(cat ../spatialprofilingtoolbox/version.txt)
 suffix="v$version""_""$datestring"
 filename="spt_$suffix.sif"
 sudo singularity build "$filename" singularity_container.def

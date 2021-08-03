15af132417bbe7a2bd42082c74eb38d3f8c4512c
Jimmy Mathews <mathewj2@mskcc.org>
2021-08-03 13:57:06 -0400

    noting upload stage

--- autorelease.sh
+++ autorelease.sh
@@ -92,5 +92,6 @@ git add spatialprofilingtoolbox/version.txt 1>/dev/null 2> stderr.txt && \
     git merge main 1>/dev/null 2> stderr.txt && \
     git push 1>/dev/null 2> stderr.txt && \
     git checkout main 1>/dev/null 2> stderr.txt && \
+    logstyle-printf "$green""Uploading to PyPI.$reset\n" && \
     python3 -m twine upload --repository spatialprofilingtoolbox dist/* && \
     logstyle-printf "$green""Done.$reset\n"

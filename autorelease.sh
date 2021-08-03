2b52c7a638749ad1032fd8d1b03b160a7684e743
Jimmy Mathews <mathewj2@mskcc.org>
2021-08-03 13:39:27 -0400

    continue fixing up pipes

--- autorelease.sh
+++ autorelease.sh
@@ -39,49 +39,51 @@ while IFS= read -r line
 done
 
 if [[ ( "$FOUND_VERSION_CHANGE" == "1" ) && ( "$FOUND_ANOTHER_CHANGE" == "1" ) ]]; then
-    printf "$red""Version has changed, but found another change. Not ready to autorelease.$reset\n"
-    echo "FAILURE"
+    echo "11"
 fi
 
 if [[ ( "$FOUND_VERSION_CHANGE" == "0" ) ]]; then
+    echo "0x"
+fi
+})
+
+if [[ "$status" == "11" ]];
+then
+    printf "$red""Version has changed, but found another change. Not ready to autorelease.$reset\n"
+    exit
+fi
+
+if [[ "$status" == "0x" ]];
+then
     printf "$red""Version has not changed, so not ready to autorelease.$reset\n"
     printf "$yellow""Maybe you need one last commit?$reset\n"
-    echo "FAILURE"
-fi
-} | grep -o "FAILURE" )
-
-if [[ "$status" == "FAILURE" ]]; then
-    echo "Got FAILURE"
-    exit
-else
-    echo "Did not get FAILURE"
     exit
 fi
 
-# printf "$green""Ready to autorelease: Version is updated, and everything else under version control is unmodified.$reset\n"
-# printf "$green""Building package.$reset\n"
-# if test -d 'dist'; then
-#     rm dist/*
-# fi
-# python3 -m build 1>/dev/null
-# printf "$green""Built:$reset\n"
-# for f in dist/*;
-# do
-#     printf "$yellow""    $f$reset\n"
-# done
-# version=$(cat spatialprofilingtoolbox/version.txt)
-# printf "$green""Committing this version:$reset$yellow v$version$reset\n"
-# git add spatialprofilingtoolbox/version.txt 1>/dev/null && \
-#     git commit -m "Autoreleasing v$version" 1>/dev/null && \
-#     git tag v$version 1>/dev/null && \
-#     git push 1>/dev/null && \
-#     git push origin v$version 1>/dev/null && \
-#     printf "$green""Pushed v$version to remote.$reset\n" && \
-#     printf "$green""Migrating updates to $release_to_branch branch.$reset\n" && \
-#     rm spatialprofilingtoolbox/version.txt && \
-#     git checkout $release_to_branch 1>/dev/null 2> stderr.txt && \
-#     git merge main 1>/dev/null  && \
-#     git push 1>/dev/null && \
-#     git checkout main 1>/dev/null 2> stderr.txt && \
-#     python3 -m twine upload --repository spatialprofilingtoolbox dist/* && \
-#     printf "$green""Done.$reset\n"
+printf "$green""Ready to autorelease: Version is updated, and everything else under version control is unmodified.$reset\n"
+printf "$green""Building package.$reset\n"
+if test -d 'dist'; then
+    rm dist/*
+fi
+python3 -m build 1>/dev/null
+printf "$green""Built:$reset\n"
+for f in dist/*;
+do
+    printf "$yellow""    $f$reset\n"
+done
+version=$(cat spatialprofilingtoolbox/version.txt)
+printf "$green""Committing this version:$reset$yellow v$version$reset\n"
+git add spatialprofilingtoolbox/version.txt 1>/dev/null && \
+    git commit -m "Autoreleasing v$version" 1>/dev/null && \
+    git tag v$version 1>/dev/null && \
+    git push 1>/dev/null && \
+    git push origin v$version 1>/dev/null && \
+    printf "$green""Pushed v$version to remote.$reset\n" && \
+    printf "$green""Migrating updates to $release_to_branch branch.$reset\n" && \
+    rm spatialprofilingtoolbox/version.txt && \
+    git checkout $release_to_branch 1>/dev/null 2> stderr.txt && \
+    git merge main 1>/dev/null  && \
+    git push 1>/dev/null && \
+    git checkout main 1>/dev/null 2> stderr.txt && \
+    python3 -m twine upload --repository spatialprofilingtoolbox dist/* && \
+    printf "$green""Done.$reset\n"

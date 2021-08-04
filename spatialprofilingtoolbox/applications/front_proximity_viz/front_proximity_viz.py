66df29a062dc6a7c6aa0109c7a0759467afbc17d
Jimmy Mathews <mathewj2@mskcc.org>
2021-08-04 00:51:01 -0400

    performance improvements in front viz

--- spatialprofilingtoolbox/applications/front_proximity_viz/front_proximity_viz.py
+++ spatialprofilingtoolbox/applications/front_proximity_viz/front_proximity_viz.py
@@ -22,14 +22,24 @@ class FrontProximityViz:
     """
     The main class of the front proximity visualization application.
     """
-    def __init__(self, distances_db_uri=None):
+    def __init__(self, distances_db_uri=None, drop_compartments=[]):
         """
         :param distances_db_uri: The URI of the database containing the output of the
             front proximity pipeline.
         :type distances_db_uri: str
-        """
-        dataframe = self.retrieve_distances_dataframe(uri=distances_db_uri)
 
+        :param drop_compartments: A list of compartments for which the corresponding
+            cells should be dropped from the table before plots are attempted. Note that
+            only 2 compartments are allowed in the table, so you may need to use this
+            parameter to drop any excess beyond 2.
+        :type drop_compartments: list
+        """
+        dataframe = self.retrieve_distances_dataframe(
+            uri=distances_db_uri,
+            drop_compartments=drop_compartments,
+        )
+
+        logger.info('Creating indices into the figure trace groups.')
         tuples_duplicated = [
             (row['sample_identifier'], row['fov_index']) for i, row in dataframe.iterrows()
         ]
@@ -51,7 +61,12 @@ class FrontProximityViz:
         all_hist_data2 = []
         all_group_labels2 = []
         sizes2 = []
+
+        logger.info('Preparing data in groups exactly as needed by the figure factory.')
+        count = 1
         for sample_identifier, fov_index in case_identifiers:
+            logger.debug('Prepared case %s/%s.', count, len(case_identifiers))
+            count += 1
             hist_data1, group_labels1 = self.get_distances_along(
                 dataframe,
                 sample_identifier=sample_identifier,
@@ -75,6 +90,7 @@ class FrontProximityViz:
             all_group_labels2 = all_group_labels2 + group_labels2
             sizes2.append(len(group_labels2))
 
+        logger.info('Creating distplots.')
         fig1 = ff.create_distplot(
             hist_data=all_hist_data1,
             group_labels=all_group_labels1,
@@ -85,6 +101,7 @@ class FrontProximityViz:
             group_labels=all_group_labels2,
             bin_size=25,
         )
+
         fig = make_subplots(
             rows=1,
             cols=2,
@@ -94,6 +111,7 @@ class FrontProximityViz:
             ),
         )
 
+        logger.info('Copying distplot data into subplottable figure.')
         case_relevant_indices = {case : [] for case in case_identifiers}
         offset = 0
         count = 0
@@ -134,6 +152,7 @@ class FrontProximityViz:
 
             offset += size
 
+        logger.info('Adding combobox and update-on-select mechanism.')
         indicator_function = {
             case : [(c in case_relevant_indices[case]) for c in range(count)] for case in case_identifiers
         }
@@ -200,16 +219,20 @@ class FrontProximityViz:
             (dataframe['other_compartment'] == other_compartment)
         ]
         grouped = df.groupby('phenotype')
-        hist_data_labels = [(list(g['distance_to_front_in_pixels']), key + ' ' + compartment) for key, g in grouped]
+        hist_data_labels = [(list(set(g['distance_to_front_in_pixels'])), key + ' ' + compartment) for key, g in grouped] # A dangerous use of "set"; try to fix instead the upstream subsampling issue
         group_labels = [row[1] for row in hist_data_labels if len(row[0]) > 2]
         hist_data = [row[0] for row in hist_data_labels if len(row[0]) > 2]
         return [hist_data, group_labels]
 
-    def retrieve_distances_dataframe(self, uri: str=None):
+    def retrieve_distances_dataframe(self, uri: str=None, drop_compartments=[]):
         """
         :param uri: The URI of the database containing the output of the front proximity
             pipeline.
         :type uri: str
+
+        :param drop_compartments: A list of compartments for which the corresponding
+            cells should be dropped from the table before plots are attempted.
+        :type drop_compartments: list
 
         :return: The table of distance-to-front values, by sample, field of view, and
             compartment pair.
@@ -217,8 +240,42 @@ class FrontProximityViz:
         """
         connection = sqlite3.connect(uri)
         table_name='cell_front_distances'
+        logger.info('Reading in dataframe from %s .', uri)
         df = pd.read_sql_query('SELECT * from ' + table_name, connection)
+        logger.info('Finished reading.')
         connection.close()
+
+        if len(drop_compartments) > 0:
+            c0 = drop_compartments[0]
+            droppable = (df['compartment'] == c0) | (df['other_compartment'] == c0)
+            if len(drop_compartments) > 1:
+                for c in drop_compartments[1:-1]:
+                    droppable = droppable | (df['compartment'] == c) | (df['other_compartment'] == c)
+            droppable = df.loc[droppable].index
+            df.drop(index=droppable, inplace=True)
+            logger.info('Dropped %s rows for compartmental membership.', len(droppable))
+            if df.shape[0] == 0:
+                logger.error('Dropped all rows!')
+                return None
+
+        grouped = df.groupby([
+                'sample_identifier',
+                'fov_index',
+                'compartment',
+                'other_compartment',
+            ]
+        )
+
+        subsample_size = 200
+        number_cells = df.shape[0]
+        if number_cells > len(grouped)*subsample_size:
+            df = grouped.sample(n=subsample_size, replace=True)
+            logger.info(
+                'Subsampled from %s to %s rows, based on a per-group limit of %s.',
+                number_cells,
+                df.shape[0],
+                subsample_size,
+            )
 
         if len(set(df['outcome_assignment'])) > 1:
             all_samples = df.copy()
@@ -236,6 +293,7 @@ class FrontProximityViz:
             'compartment',
             'other_compartment',
         ]).reset_index(drop=True)
+        logger.info('Finished preprocessing distances table.')
         return df
 
     def start_showing(self):

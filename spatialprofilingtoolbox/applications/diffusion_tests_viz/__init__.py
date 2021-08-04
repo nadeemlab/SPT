26e8af1f02a155e65bbd661ebf9847a859867514
Jimmy Mathews <mathewj2@mskcc.org>
2021-08-03 21:46:42 -0400

    adding module level docstrings for applications

--- spatialprofilingtoolbox/applications/diffusion_tests_viz/__init__.py
+++ spatialprofilingtoolbox/applications/diffusion_tests_viz/__init__.py
@@ -1,2 +1,7 @@
+"""
+This application plots the result of outcome-to-outcome statistical testing
+involving the diffusion distances, by phenotype and by "temporal offset"
+pertaining to the diffusion Markov chain.
+"""
 
 from .diffusion_tests_viz import DiffusionTestsViz
--- spatialprofilingtoolbox/applications/front_proximity_viz/__init__.py
+++ spatialprofilingtoolbox/applications/front_proximity_viz/__init__.py
@@ -1,6 +1,6 @@
 """
-This application plots the distance-to-front metrics calculated by the front proximity
-pipeline.
+This application plots the distance-to-front metrics calculated by the front
+proximity pipeline.
 """
 
 from .front_proximity_viz import FrontProximityViz

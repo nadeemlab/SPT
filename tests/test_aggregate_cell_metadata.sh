ae6404a2c30048d24357c0b98fd21cfeb42607c2
James Mathews <mail@jmath.read-books.org>
2021-08-12 19:50:21 -0400

    fix cli call

--- tests/test_aggregate_cell_metadata.sh
+++ tests/test_aggregate_cell_metadata.sh
@@ -2,7 +2,7 @@
 
 cp sample_config_file_proximity.txt .spt_pipeline.config
 spt-aggregate-cell-data
-spt-aggregate-cell-data 5
-spt-aggregate-cell-data 10
-spt-aggregate-cell-data 57
+spt-aggregate-cell-data --max-per-sample 5
+spt-aggregate-cell-data --max-per-sample 10
+spt-aggregate-cell-data --max-per-sample 57
 rm .spt_pipeline.config

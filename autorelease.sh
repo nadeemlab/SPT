37e3111d4806acd4fe3ac9e1abf4075be1888e5d
Jimmy Mathews <mathewj2@mskcc.org>
2021-08-17 15:21:57 -0400

    padding

--- autorelease.sh
+++ autorelease.sh
@@ -14,8 +14,11 @@ script_file=$(echo "$0" | grep -oE "[a-zA-Z0-9_]+.sh$")
 
 function logstyle-printf() {
     elapsed_minutes=$(bc <<< "scale=2; $SECONDS/60")
+    char_width=${#elapsed_minutes}
+    padding=$(( char_width + 2 ))
     units="m"
-    printf "$source_note_color[$script_file $elapsed_minutes$units%-5s]$reset $1"
+    control_char="s"
+    printf "$source_note_color[$script_file $elapsed_minutes$units%-$padding$control_char]$reset $1"
 }
 
 current_branch=$(git branch | grep '^* ')

8dad0124e63b6f016806e573362414188f3282c3
Jimmy Mathews <mathewj2@mskcc.org>
2021-08-16 23:17:22 -0400

    simplify test

--- tests/test_analyze_results_diffusion.sh
+++ tests/test_analyze_results_diffusion.sh
@@ -2,5 +2,4 @@
 
 export DEBUG=1
 cp sample_config_file_diffusion.txt .spt_pipeline.config
-spt-analyze-results > logs/test_analysis_diffusion.out 2>&1 &
-tail -f logs/test_analysis_diffusion.out
+spt-analyze-results > logs/test_analysis_diffusion.out

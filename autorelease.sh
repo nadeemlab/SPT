38da373732e5bc9dfcadc9c864752e0443985887
Jimmy Mathews <mathewj2@mskcc.org>
2021-08-17 15:49:55 -0400

    newline calc

--- autorelease.sh
+++ autorelease.sh
@@ -32,14 +32,19 @@ function logstyle-printf() {
     control_char="s"
     bar="$script_file %-$padding$control_char$elapsed $units"
 
-    printf "$source_note_color[$bar]$reset $MESSAGE"
+    if [[ "$TIME_NEXT" == "1" ]];
+    then
+        printf "$source_note_color[$bar]$reset $MESSAGE"
+    fi
 
     if [[ "$2" == "timed-command" ]];
     then
         TIME_NEXT=1
         start_time=$SECONDS
         SAVED_MESSAGE="$1"
-        logstyle-printf $1
+        logstyle-printf "$1 "
+    else
+        printf "$source_note_color[$bar]$reset $MESSAGE"
     fi
 }
 
@@ -83,27 +88,27 @@ fi
 
 if [[ "$status" == "11" ]];
 then
-    logstyle-printf "$red""Version has changed, but found another change. Not ready to autorelease.$reset\n"
+    logstyle-printf "$red""Version has changed, but found another change. Not ready to autorelease.$reset"
     exit
 fi
 
 if [[ "$status" == "0x" ]];
 then
-    logstyle-printf "$red""Version has not changed, so not ready to autorelease.$reset\n"
-    logstyle-printf "$yellow""Maybe you need one last commit?$reset\n"
+    logstyle-printf "$red""Version has not changed, so not ready to autorelease.$reset"
+    logstyle-printf "$yellow""Maybe you need one last commit?$reset"
     exit
 fi
 
-logstyle-printf "$green""Ready to autorelease: Version is updated, and everything else under version control is unmodified.$reset\n"
-logstyle-printf "$green""Building package.$reset\n"
+logstyle-printf "$green""Ready to autorelease: Version is updated, and everything else under version control is unmodified.$reset"
+logstyle-printf "$green""Building package.$reset"
 if test -d 'dist'; then
     rm dist/*
 fi
 python3 -m build 1>/dev/null
-logstyle-printf "$green""Built:$reset\n"
+logstyle-printf "$green""Built:$reset"
 for f in dist/*;
 do
-    logstyle-printf "$yellow""    $f$reset\n"
+    logstyle-printf "$yellow""    $f$reset"
 done
 
 installed=$(pip3 freeze | grep spatialprofilingtoolbox | wc -l)
@@ -117,7 +122,7 @@ function clean_exit() {
     exit
 }
 
-logstyle-printf "$green""Installing wheel into virtual environment.$reset\n"
+logstyle-printf "$green""Installing wheel into virtual environment.$reset"
 python3 -m venv venv
 source venv/bin/activate
 for wheel in dist/*.whl;
@@ -127,18 +132,18 @@ done
 
 pip install pytest 1>/dev/null 2> stderr.txt
 
-logstyle-printf "$green""Running unit tests.$reset\n"
+logstyle-printf "$green""Running unit tests.$reset"
 cd tests/
 outcome=$(python -m pytest -q . | tail -n1 | grep "[0-9]\+ \(failed\|errors\)")
 if [[ ! "$outcome" == "" ]]; then
-    logstyle-printf "$red""Something went wrong in unit tests.$reset\n"
+    logstyle-printf "$red""Something went wrong in unit tests.$reset"
     clean_exit
 fi
 
-logstyle-printf "$green""Running integration tests.$reset\n"
+logstyle-printf "$green""Running integration tests.$reset"
 outcome=$(./tests_integration.sh | tail -n1 | grep "all [0-9]\+ SPT workflows integration tests passed in")
 if [[ "$outcome" == "" ]]; then
-    logstyle-printf "$red""Something went wrong in integration tests.$reset\n"
+    logstyle-printf "$red""Something went wrong in integration tests.$reset"
     clean_exit
 fi
 
@@ -155,19 +160,19 @@ exit
 exit
 
 version=$(cat spatialprofilingtoolbox/version.txt)
-logstyle-printf "$green""Committing this version:$reset$bold_cyan v$version$reset\n"
+logstyle-printf "$green""Committing this version:$reset$bold_cyan v$version$reset"
 git add spatialprofilingtoolbox/version.txt 1>/dev/null 2> stderr.txt && \
     git commit -m "Autoreleasing v$version" 1>/dev/null 2> stderr.txt && \
     git tag v$version 1>/dev/null 2> stderr.txt && \
     git push 1>/dev/null 2> stderr.txt && \
     git push origin v$version 1>/dev/null 2> stderr.txt && \
-    logstyle-printf "$green""Pushed ""$bold_cyan""v$version$reset$green on remote branch$reset$yellow main$reset$green .\n" && \
-    logstyle-printf "$green""Migrating updates to $reset$yellow$release_to_branch$reset$green branch.$reset\n" && \
+    logstyle-printf "$green""Pushed ""$bold_cyan""v$version$reset$green on remote branch$reset$yellow main$reset$green ." && \
+    logstyle-printf "$green""Migrating updates to $reset$yellow$release_to_branch$reset$green branch.$reset" && \
     rm spatialprofilingtoolbox/version.txt && \
     git checkout $release_to_branch 1>/dev/null 2> stderr.txt && \
     git merge main 1>/dev/null 2> stderr.txt && \
     git push 1>/dev/null 2> stderr.txt && \
     git checkout main 1>/dev/null 2> stderr.txt && \
-    logstyle-printf "$green""Uploading to PyPI.$reset\n" && \
+    logstyle-printf "$green""Uploading to PyPI.$reset" && \
     python3 -m twine upload --repository spatialprofilingtoolbox dist/* && \
-    logstyle-printf "$green""Done.$reset\n"
+    logstyle-printf "$green""Done.$reset"

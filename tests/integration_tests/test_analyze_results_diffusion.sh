00beccdbed21692b228661b5a3ce794b9e2235f3
Jimmy Mathews <mathewj2@mskcc.org>
2021-08-17 12:03:17 -0400

    simpler integration tests lineup

--- tests/integration_tests/test_analyze_results_diffusion.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export DEBUG=1
-cp example_config_files/diffusion.txt .spt_pipeline.config
-spt-analyze-results > logs/test_analysis_diffusion.out
--- tests/integration_tests/test_analyze_results_proximity.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export DEBUG=1
-cp example_config_files/proximity.txt .spt_pipeline.config
-spt-analyze-results > logs/test_analysis_proximity.out

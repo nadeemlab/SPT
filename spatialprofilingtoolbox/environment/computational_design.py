33774d5aba4d703a0f8c2a1bd3e25cd3a0e254b1
James Mathews <mail@jmath.read-books.org>
2021-09-24 13:11:25 -0400

    adding dichotomization preprocessing step capability

--- spatialprofilingtoolbox/environment/computational_design.py
+++ spatialprofilingtoolbox/environment/computational_design.py
@@ -1,25 +1,47 @@
 
+from .dichotomization import Dichotomizer
+from .log_formats import colorized_logger
+
+logger = colorized_logger(__name__)
+
 
 class ComputationalDesign:
     """
-    This is an object to collect together any metadata that is specific to a
-    particular pipeline/workflow's computation stage. This, as opposed to the input
-    data parsing stage, for example.
+    Subclass this object to collect together any metadata that is specific to a
+    particular pipeline/workflow's computation stage.
     """
-    def __init__(self, **kwargs):
-        pass
+    def __init__(self, dichotomize: bool=False, **kwargs):
+        self.dichotomize = dichotomize
+
+    def preprocess(self, table, dataset_design):
+        if self.dichotomize:
+            for phenotype in dataset_design.get_elementary_phenotype_names():
+                intensity = dataset_design.get_intensity_column_name(phenotype_name)
+                if not intensity in table.columns:
+                    dataset_design.add_combined_intensity_column(table, phenotype)
+                Dichotomizer.dichotomize(
+                    phenotype,
+                    table,
+                    dataset_design=dataset_design,
+                )
+                feature = dataset_design.get_feature_name(phenotype)
+                number_positives = sum(cells[feature])
+                logger.info(
+                    'Dichotomization column "%s" written. %s positives.',
+                    feature,
+                    number_positives,
+                )
 
     def get_database_uri(self):
         """
         Each computational workflow may request persistent storage of intermediate data.
-        The implementation class should provide the URI of the database in which to store
-        this data.
+        The implementation class should provide the URI of the database in which to
+        store this data.
 
-        Currently, only local sqlite databases are supported. Future version may support
-        remote SQL database connections.
+        Currently, only local sqlite databases are supported. Future version may
+        support remote SQL database connections.
 
-        Returns:
-            str:
-                The Uniform Resource Identifier (URI) identifying the database.
+        :return: The Uniform Resource Identifier (URI) identifying the database.
+        :rtype: str
         """
         pass

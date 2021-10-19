62ccdbb3b7e0d3409f48ff8b11e3ca98029c1730
James Mathews <mail@jmath.read-books.org>
2021-10-19 10:41:33 -0400

    normalize csv output for checksum checking

--- tests/integration_tests/density_pipeline_with_intensities.sh
+++ tests/integration_tests/density_pipeline_with_intensities.sh
@@ -4,4 +4,6 @@ export DEBUG=1
 cp integration_tests/example_config_files/density_with_intensities.json .spt_pipeline.json
 source test_run_pipeline.sh
 
-check_output_file_sum dbf42ceb177c23de7f56ff876320a0bad8bf2ba6bfe08a56c25ba3d02085b848 output/density_tests.csv
+cat output/density_tests.csv | sort > output/density_tests.csv.normalized
+check_output_file_sum 3d3c99ce6bed46aff5450991123bfa8ab04be977b1a8ff24b5c96f17f5dc133b output/density_tests.csv.normalized
+rm output/density_tests.csv.normalized

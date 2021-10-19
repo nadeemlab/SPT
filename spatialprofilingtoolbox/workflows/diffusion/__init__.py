e1bf01a338cfd7412d595421040a5d73ae687cde
James Mathews <mail@jmath.read-books.org>
2021-10-19 10:47:44 -0400

    updating checksum in test file

--- spatialprofilingtoolbox/workflows/diffusion/__init__.py
+++ spatialprofilingtoolbox/workflows/diffusion/__init__.py
@@ -1,12 +1,15 @@
 """
-The core of this module takes as input a collection of points, and generates the associated diffusion map and diffusion Markov chain, with the aim of producing features that are characteristic of the input geometry.
+The core of this module takes as input a collection of points, and generates
+the associated diffusion map and diffusion Markov chain, with the aim of
+producing features that are characteristic of the input geometry.
 
-Taken as a whole the diffusion analysis pipeline provides statistical test results and figures that assess the efficacy of diffusion-related metrics as discriminators of selected correlates.
+Taken as a whole the diffusion analysis pipeline provides statistical test
+results and figures that assess the efficacy of diffusion-related metrics as
+discriminators of selected correlates.
 """
+
 from ...environment.workflow_modules import WorkflowModules
-
 from ...dataset_designs.multiplexed_imaging.halo_cell_metadata_design import HALOCellMetadataDesign
-
 from .job_generator import DiffusionJobGenerator
 from .analyzer import DiffusionAnalyzer
 from .computational_design import DiffusionDesign
--- tests/integration_tests/diffusion_pipeline.sh
+++ tests/integration_tests/diffusion_pipeline.sh
@@ -5,5 +5,5 @@ cp integration_tests/example_config_files/diffusion.json .spt_pipeline.json
 source test_run_pipeline.sh
 
 cat output/diffusion_distance_tests.csv | sort > output/diffusion_distance_tests.csv.normalized
-check_output_file_sum da3738229fb8332de4a061a8e30eff1b31930443cd0526f791019d6f08de331d output/diffusion_distance_tests.csv.normalized
+check_output_file_sum c9c02c8f84a8dae34cdbcc2293fa60215714222ae59ddd7f4daae023347e4b57 output/diffusion_distance_tests.csv.normalized
 rm output/diffusion_distance_tests.csv.normalized

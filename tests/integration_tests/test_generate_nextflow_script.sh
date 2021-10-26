5c281f7e2c23a40401c84641d73648d1371b283e
James Mathews <mail@jmath.read-books.org>
2021-10-26 14:34:08 -0400

    updating tests to involve nf

--- tests/integration_tests/test_generate_nextflow_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp integration_tests/example_config_files/proximity.json .spt_pipeline.json
-spt-pipeline generate-jobs
-rm .spt_pipeline.json
--- tests/test_run_pipeline.sh
+++ tests/test_run_pipeline.sh
@@ -1,27 +1,3 @@
 #!/bin/bash
 
-check_output_file_sum() {
-    expected=$1
-    file=$2
-    sum=$(sha256sum $file | grep -o "^$expected")
-    if [ "$expected" != "$sum" ];
-    then
-        echo "SHA256 sum is wrong on file $file ."
-        exit 1
-    else
-        echo "SHA256 sum checked out for file $file ."
-    fi
-}
-
-spt-pipeline generate-jobs
-for script in schedule_*.sh;
-do
-    chmod +x $script
-    ./$script
-done
-spt-pipeline aggregate-results &> logs/integration.txt
-rm .spt_pipeline.json
-for script in schedule_*.sh;
-do
-    rm $script
-done
+nextflow spt_pipeline.nf

47461fdc28954eb1924f61223781ef78840c43e0
James Mathews <mail@jmath.read-books.org>
2021-10-27 09:22:23 -0400

    deprecating utility script for manifest aggregation

--- tests/integration_tests/aggregate_cell_metadata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp integration_tests/example_config_files/proximity.json .spt_pipeline.json
-spt-aggregate-cell-data
-spt-aggregate-cell-data --max-per-sample 5
-spt-aggregate-cell-data --max-per-sample 10
-spt-aggregate-cell-data --max-per-sample 57
-rm .spt_pipeline.json
-rm cell_metadata_*.csv

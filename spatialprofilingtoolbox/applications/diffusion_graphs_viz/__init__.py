b08dcd169ea406534c008b1c772586140cbed9eb
James Mathews <mail@jmath.read-books.org>
2022-05-02 15:53:26 -0400

    deprecating diffusion wf

--- spatialprofilingtoolbox/applications/diffusion_graphs_viz/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-This application shows graphs representing the diffusion distance between cells.
-"""
-
-from .diffusion_graphs_viz import DiffusionGraphsViz
--- spatialprofilingtoolbox/applications/diffusion_tests_viz/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-This application plots the result of outcome-to-outcome statistical testing
-involving the diffusion distances.
-"""
-
-from .diffusion_tests_viz import DiffusionTestsViz

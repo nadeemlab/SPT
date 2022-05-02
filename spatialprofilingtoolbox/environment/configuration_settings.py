59ec73f228d8441e21d0404612d2dcdd51b688fe
James Mathews <mail@jmath.read-books.org>
2022-05-02 17:04:44 -0400

    remove extra variable declaration

--- spatialprofilingtoolbox/environment/configuration_settings.py
+++ spatialprofilingtoolbox/environment/configuration_settings.py
@@ -4,21 +4,13 @@ from ..workflows.density import components as density_workflow
 from ..workflows.phenotype_proximity import components as phenotype_proximity_workflow
 from ..workflows.front_proximity import components as front_proximity_workflow
 
-from ..workflows.density import name as density_name
-from ..workflows.phenotype_proximity import name as phenotype_proximity_name
-from ..workflows.front_proximity import name as front_proximity_name
-
 workflows = {
     **density_workflow,
     **phenotype_proximity_workflow,
     **front_proximity_workflow,
 }
 
-workflow_names = [
-    density_name,
-    phenotype_proximity_name,
-    front_proximity_name,
-]
+workflow_names = list(workflow.keys())
 
 config_filename = '.spt_pipeline.json'
 

eeba1030401343e7cf39eafc1573d0c1e42fbb8f
James Mathews <mail@jmath.read-books.org>
2022-06-01 15:05:49 -0400

    cascade issue

--- spatialprofilingtoolbox/data_model/drop_views.sql
+++ spatialprofilingtoolbox/data_model/drop_views.sql
@@ -1,9 +1,9 @@
 
-DROP MATERIALIZED VIEW IF EXISTS cell_count_by_study_specimen ;
-DROP MATERIALIZED VIEW IF EXISTS marker_positive_cell_count_by_study_specimen ;
+DROP MATERIALIZED VIEW IF EXISTS cell_count_by_study_specimen CASCADE ;
+DROP MATERIALIZED VIEW IF EXISTS marker_positive_cell_count_by_study_specimen CASCADE ;
 
-DROP VIEW IF EXISTS fraction_stats_by_marker_study ;
-DROP VIEW IF EXISTS fraction_by_marker_study_specimen ;
+DROP VIEW IF EXISTS fraction_stats_by_marker_study CASCADE ;
+DROP VIEW IF EXISTS fraction_by_marker_study_specimen CASCADE ;
 
 
 --Below are to be deprecated

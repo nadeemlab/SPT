5b3feee20dd45cc93440a159e513f088df7a2df9
Jimmy Mathews <mathewj2@mskcc.org>
2022-08-18 00:18:13 -0400

    basic usage demonstrated in client script

--- client.py
+++ client.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python3
-
 import socket
 
 host_ip = '127.0.0.1'
 server_port = 8016
-
-# Initialize a TCP client socket using SOCK_STREAM
 tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 try:
-    # Establish connection to TCP server and exchange data
     tcp_client.connect((host_ip, server_port))
-
     data = input()
-    tcp_client.sendall(data.encode())
-
-    # Read data from the TCP server and close the connection
+    names = data.split(' ')
+    record_separator = chr(30)
+    record = record_separator.join(names)
+    tcp_client.sendall(record.encode('utf-8'))
     received = tcp_client.recv(8)
 finally:
     tcp_client.close()
 
-print ("Bytes Sent:     {}".format(data))
-print ("Bytes Received: {}".format(received.decode()))
+count = int.from_bytes(received, 'little')
+print('Count: %s' % count)

220faaf6415cba23658cd5902fb073ca4380d320
Jimmy Mathews <mathewj2@mskcc.org>
2022-08-22 23:37:13 -0400

    adding support for positive negative in server and client

--- apiserver/app/counts_service.py
+++ apiserver/app/counts_service.py
@@ -8,15 +8,20 @@ class CountRequester:
         self.tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.tcp_client.connect((host, port))
 
-    def get_counts(self, partial_signature_channels, study_name):
-        query = self.form_query([self.sanitize_token(c) for c in partial_signature_channels], self.sanitize_token(study_name))
+    def get_counts(self, positive_signature_channels, negative_signature_channels, study_name):
+        query = self.form_query(
+            [self.sanitize_token(c) for c in positive_signature_channels],
+            [self.sanitize_token(c) for c in negative_signature_channels],
+            self.sanitize_token(study_name),
+        )
         self.tcp_client.sendall(query)
         return self.get_counts_by_specimen()
 
-    def form_query(self, partial_signature_channels, study_name):
-        return self.get_record_separator().join(
-            [study_name] + partial_signature_channels
-        ).encode('utf-8')
+    def form_query(self, positive_signature_channels, negative_signature_channels, study_name):
+        group1 = study_name
+        group2 = self.get_record_separator().join(positive_signature_channels)
+        group3 = self.get_record_separator().join(negative_signature_channels)
+        return self.get_group_separator().join([group1, group2, group3]).encode('utf-8')
 
     def get_counts_by_specimen(self):
         received = None
@@ -29,7 +34,10 @@ class CountRequester:
         return json.loads(buffer.decode('utf-8'))
 
     def sanitize_token(self, text):
-        return re.sub(self.get_record_separator(), ' ', text)
+        return re.sub('[' + self.get_record_separator() + self.get_group_separator() + ']', ' ', text)
+
+    def get_group_separator(self):
+        return chr(29)
 
     def get_record_separator(self):
         return chr(30)

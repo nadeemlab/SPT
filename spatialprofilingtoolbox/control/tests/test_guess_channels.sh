f2784a89e9e3d322c8772be3d27015628d3d4054
Jimmy Mathews <mathewj2@mskcc.org>
2022-08-26 01:50:00 -0400

    update another script call

--- spatialprofilingtoolbox/control/tests/test_guess_channels.sh
+++ spatialprofilingtoolbox/control/tests/test_guess_channels.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-spt-guess-channels \
+spt control guess-channels \
  ./data/2779f21192cb0ce1479b2bf7fb20ebba.csv \
  ./data/2cc18c6561b05abb1a1a95d15130a1d3.csv \
  ./data/33c794a479e571ae50518546555b9480.csv \

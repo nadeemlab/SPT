7291851f68b9083f184520c8052bd393a25458b0
James Mathews <mail@jmath.read-books.org>
2022-09-07 13:30:32 -0400

    strip out docs

--- spatialprofilingtoolbox/__init__.py
+++ spatialprofilingtoolbox/__init__.py
@@ -1,7 +1,3 @@
-"""
-This is the Spatial Profiling Toolbox package. The source code is available
-`here <https://github.com/nadeemlab/SPT>`_.
-"""
 import re
 import pkgutil
 
@@ -27,24 +23,15 @@ logger = colorized_logger(__name__)
 __version__ = get_version()
 
 def get_dataset_design(workflow=None, **kwargs):
-    """
-    Exposes design parameters to scripts.
-    """
     return get_workflow(workflow).dataset_design(**kwargs)
 
 def get_computational_design(workflow=None, **kwargs):
-    """
-    Exposes design parameters to scripts.
-    """
     ComputationalDesign = get_workflow(workflow).computational_design
     dataset_design = get_dataset_design(workflow = workflow, **kwargs)
     computational_design = ComputationalDesign(dataset_design = dataset_design, **kwargs)
     return computational_design
 
 def get_initializer(workflow=None, **kwargs):
-    """
-    Exposes pipeline initializer to scripts.
-    """
     dataset_design = get_dataset_design(workflow = workflow, **kwargs)
     computational_design = get_computational_design(workflow = workflow, **kwargs)
     Initializer = get_workflow(workflow).initializer
@@ -55,9 +42,6 @@ def get_initializer(workflow=None, **kwargs):
     )
 
 def get_core_job(workflow=None, **kwargs):
-    """
-    Exposes pipeline core jobs to scripts.
-    """
     dataset_design = get_dataset_design(workflow = workflow, **kwargs)
     computational_design = get_computational_design(workflow = workflow, **kwargs)
     CoreJob = get_workflow(workflow).core_job
@@ -68,9 +52,6 @@ def get_core_job(workflow=None, **kwargs):
     )
 
 def get_integrator(workflow=None, **kwargs):
-    """
-    Exposes pipeline analysis integrators to scripts.
-    """
     computational_design = get_computational_design(workflow = workflow, **kwargs)
 
     Integrator = get_workflow(workflow).integrator

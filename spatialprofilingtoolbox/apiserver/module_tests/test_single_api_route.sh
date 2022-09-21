682f9e04f9de3b252cea5e14519a397dfae4a507
Jimmy Mathews <mathewj2@mskcc.org>
2022-09-21 00:43:56 -0400

    add compose for apiserver

---
+++ spatialprofilingtoolbox/apiserver/module_tests/test_single_api_route.sh
@@ -0,0 +1,14 @@
+
+curl -s http://127.0.0.1:8080/specimen-measurement-study-names | python -m json.tool > names.json
+
+diff module_tests/expected_specimen_measurement_study_names.json names.json
+status=$?
+[ $status -eq 0 ] && echo "API server served correct measurement study names." || echo "API query for measurement study names failed."
+rm names.json
+
+if [ $status -eq 0 ];
+then
+    exit 0
+else
+    exit 1
+fi

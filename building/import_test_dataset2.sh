0edbf73f1b548d72b5d7cc09295fe3150e6226c0
James Mathews <mail@jmath.read-books.org>
2022-11-02 14:59:53 -0400

    commit test data importers

---
+++ building/import_test_dataset2.sh
@@ -0,0 +1,3 @@
+
+spt workflow configure --local --input-path spatialprofilingtoolbox/test_data/adi_preprocessed_tables/dataset2/ --workflow='HALO import' --database-config-file spatialprofilingtoolbox/db/.spt_db.config.local
+nextflow run .

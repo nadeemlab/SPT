9864449d4eebb110b18c31260e38a5099a568524
Carlin Liao <liaoc2@mskcc.org>
2022-12-01 16:46:22 -0500

    removed dashes in cggnn

--- spatialprofilingtoolbox/cggnn/unit_tests/test_image_runs_properly.sh
+++ spatialprofilingtoolbox/cggnn/unit_tests/test_image_runs_properly.sh
@@ -1,6 +1,6 @@
 pip freeze | grep dgl
 status=$?
-[ $status -eq 0 ] || echo "Docker image for cg-gnn did not build and run properly."
+[ $status -eq 0 ] || echo "Docker image for cggnn did not build and run properly."
 
 if [ $status -eq 0 ];
 then

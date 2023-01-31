4cbc6b0f772c3a17d555ec5cf1192f4ca3d8f92c
Jimmy Mathews <mathewj2@mskcc.org>
2023-01-30 23:35:23 -0500

    Start breaking down massive branching cell manifest parsing.

--- spatialprofilingtoolbox/workflow/source_file_adi_parsing/cell_manifests.py
+++ spatialprofilingtoolbox/workflow/source_file_adi_parsing/cell_manifests.py
@@ -26,6 +26,160 @@ class CellManifestsParser(SourceToADIParser):
         super().__init__(fields, **kwargs)
         self.using_intensities = False
 
+    def parse_cell_manifest(self, cursor, filename, channel_symbols,
+                            initial_indices):
+        histological_structure_identifier_index = initial_indices['structure']
+        shape_file_identifier_index = initial_indices['shape file']
+        sha256_hash = compute_sha256(filename)
+        cells = pd.read_csv(filename, sep=',', na_filter=False).drop_duplicates()
+        count = self.get_number_known_cells(sha256_hash, cursor)
+        if count > 0 and count != cells.shape[0]:
+            logger.warning(
+                ('Found %s cells but %s already known from data source file "%s". '
+                    ' You may need to drop bad cell records from '
+                    'histological_structure_identification table, or check the source '
+                    'data file\'s integrity. For now, skipping this source file.'),
+                cells.shape[0],
+                count,
+                sha256_hash,
+            )
+            return {'structure' : histological_structure_identifier_index,
+                    'shape file' : shape_file_identifier_index}
+        if count == cells.shape[0]:
+            logger.debug(
+                ('Already found exactly %s cells recorded from data source '
+                    ' file "%s". Skipping this file.'
+                    ),
+                count,
+                sha256_hash,
+            )
+            return {'structure' : histological_structure_identifier_index,
+                    'shape file' : shape_file_identifier_index}
+        if count == 0:
+            timer.record_timepoint('Retrieved and hashed a cell manifest')
+            chunk_size = 100000
+            for start in range(0, cells.shape[0], chunk_size):
+                timer.record_timepoint('Starting a chunk')
+                batch_cells_reference = cells.iloc[start:start + chunk_size]
+                batch_cells = batch_cells_reference.reset_index(drop=True)
+                records = {
+                    'histological_structure': [],
+                    'shape_file': [],
+                    'histological_structure_identification': [],
+                    'expression_quantification': [],
+                }
+                timer.record_timepoint('Subset cells dataframe on chunk')
+                if self.using_intensities:
+                    intensities = {
+                        symbol: dataset_design.get_combined_intensity(
+                            batch_cells, symbol)
+                        for symbol in channel_symbols
+                    }
+                    timer.record_timepoint('Retrieved intensities on chunk')
+                else:
+                    intensities = None
+
+                if all([dataset_design.get_feature_name(symbol) in batch_cells.columns
+                        for symbol in channel_symbols]):
+                    feature_names = {symbol: dataset_design.get_feature_name(
+                        symbol) for symbol in channel_symbols}
+                else:
+                    logger.warning(
+                        'Exact feature names not found in tables. Trying with underscores...')
+                    if all([re.sub(' ', '_', dataset_design.get_feature_name(symbol))
+                            in batch_cells.columns for symbol in channel_symbols]):
+                        feature_names = {symbol: re.sub(' ', '_',
+                                                        dataset_design.get_feature_name(
+                                                            symbol)
+                                                        ) for symbol in channel_symbols}
+                    else:
+                        logger.warning('Not even with underscores.')
+                        missing = [symbol for symbol in channel_symbols
+                                    if not dataset_design.get_feature_name(symbol)
+                                    in batch_cells.columns]
+                        logger.warning('Specifically, the following features not found:')
+                        for symbol in missing:
+                            logger.warning(symbol)
+                        raise ValueError('Could not find feature names in cell manifest.')
+
+                discretizations = {
+                    symbol: batch_cells[feature_names[symbol]]
+                    for symbol in channel_symbols
+                }
+                timer.record_timepoint('Retrieved discretizations on chunk')
+
+                logger.debug('Starting batch of cells that begins at index %s.', start)
+                timer.record_timepoint('Started per-cell iteration')
+                for j, cell in batch_cells.iterrows():
+                    histological_structure_identifier = str(
+                        histological_structure_identifier_index)
+                    histological_structure_identifier_index += 1
+                    shape_file_identifier = str(shape_file_identifier_index)
+                    shape_file_identifier_index += 1
+                    timer.record_timepoint('Beginning of one cell iteration')
+                    shape_file_contents = self.create_shape_file(cell, dataset_design)
+                    timer.record_timepoint('Created shapefile contents')
+                    records['histological_structure'].append((
+                        histological_structure_identifier,
+                        'cell',
+                    ))
+                    records['shape_file'].append((
+                        shape_file_identifier,
+                        'ESRI Shapefile SHP',
+                        shape_file_contents,
+                    ))
+                    records['histological_structure_identification'].append((
+                        histological_structure_identifier,
+                        sha256_hash,
+                        shape_file_identifier,
+                        '\\N',
+                        '',
+                        '',
+                        '',
+                    ))
+                    for symbol in channel_symbols:
+                        target = chemical_species_identifiers_by_symbol[symbol]
+                        if not intensities is None:
+                            quantity = intensities[symbol][j]
+                            if quantity in [None, '']:
+                                continue
+                            quantity = str(quantity)
+                        else:
+                            quantity = '-1'
+                        discrete_value = discretizations[symbol][j]  # type: ignore
+                        records['expression_quantification'].append((
+                            histological_structure_identifier,
+                            target,
+                            quantity,
+                            '',
+                            '',
+                            'positive' if discrete_value == 1 else 'negative',
+                            '',
+                        ))
+
+                table_names = [
+                    'histological_structure',
+                    'shape_file',
+                    'histological_structure_identification',
+                    'expression_quantification',
+                ]
+                for tablename in table_names:
+                    timer.record_timepoint('Started encoding one chunk')
+                    values_file_contents = '\n'.join([
+                        '\t'.join(r) for r in records[tablename]
+                    ]).encode('utf-8')
+                    timer.record_timepoint('Started inserting chunk into local memory')
+                    with mmap.mmap(-1, len(values_file_contents)) as memmap:
+                        memmap.write(values_file_contents)
+                        memmap.seek(0)
+                        timer.record_timepoint('Started copy from command for bulk insertion')
+                        cursor.copy_from(memmap, tablename)
+                    timer.record_timepoint('Finished inserting one chunk')
+        logger.info('Parsed records for %s cells from "%s".', cells.shape[0], sha256_hash)
+        return {'structure' : histological_structure_identifier_index,
+                'shape file' : shape_file_identifier_index}
+
+
     def parse(self,
               connection,
               dataset_design,
@@ -42,9 +196,8 @@ class CellManifestsParser(SourceToADIParser):
         timer = PerformanceTimer()
         timer.record_timepoint('Initial')
         file_metadata = pd.read_csv(file_manifest_file, sep='\t')
-        halo_data_type = dataset_design.get_cell_manifest_descriptor()
         cell_manifests = file_metadata[
-            file_metadata['Data type'] == halo_data_type
+            file_metadata['Data type'] == dataset_design.get_cell_manifest_descriptor()
         ]
         recognized_channel_symbols = dataset_design.get_elementary_phenotype_names()
         missing_channel_symbols = set(chemical_species_identifiers_by_symbol.keys()
@@ -61,167 +214,29 @@ class CellManifestsParser(SourceToADIParser):
         timer.record_timepoint('Cursor opened')
         histological_structure_identifier_index = self.get_next_integer_identifier(
             'histological_structure', cursor)
-        shape_file_identifier_index = self.get_next_integer_identifier(
-            'shape_file', cursor)
+        shape_file_identifier_index = self.get_next_integer_identifier('shape_file', cursor)
         timer.record_timepoint('Retrieved next integer identifiers')
+        initial_indices = {
+            'structure' : histological_structure_identifier_index,
+            'shape file' : shape_file_identifier_index,
+        }
+        final_indices = {}
         file_count = 1
         for _, cell_manifest in cell_manifests.iterrows():
             logger.debug(
-                'Considering contents of "%s" file "%s".',
-                halo_data_type,
+                'Considering contents of file "%s".',
                 cell_manifest['File ID'],
             )
             filename = get_input_filename_by_identifier(
                 input_file_identifier=cell_manifest['File ID'],
                 file_manifest_filename=file_manifest_file,
             )
-            sha256_hash = compute_sha256(filename)
-            cells = pd.read_csv(filename, sep=',', na_filter=False).drop_duplicates()
-            count = self.get_number_known_cells(sha256_hash, cursor)
-            if count > 0 and count != cells.shape[0]:
-                logger.warning(
-                    ('Found %s cells but %s already known from data source file "%s". '
-                     ' You may need to drop bad cell records from '
-                     'histological_structure_identification table, or check the source '
-                     'data file\'s integrity. For now, skipping this source file.'),
-                    cells.shape[0],
-                    count,
-                    sha256_hash,
-                )
-                continue
-            if count == cells.shape[0]:
-                logger.debug(
-                    ('Already found exactly %s cells recorded from data source '
-                     ' file "%s". Skipping this file.'
-                     ),
-                    count,
-                    sha256_hash,
-                )
-                continue
-            if count == 0:
-                timer.record_timepoint('Retrieved and hashed a cell manifest')
-                chunk_size = 100000
-                for start in range(0, cells.shape[0], chunk_size):
-                    timer.record_timepoint('Starting a chunk')
-                    batch_cells_reference = cells.iloc[start:start + chunk_size]
-                    batch_cells = batch_cells_reference.reset_index(drop=True)
-                    records = {
-                        'histological_structure': [],
-                        'shape_file': [],
-                        'histological_structure_identification': [],
-                        'expression_quantification': [],
-                    }
-                    timer.record_timepoint('Subset cells dataframe on chunk')
-                    if self.using_intensities:
-                        intensities = {
-                            symbol: dataset_design.get_combined_intensity(
-                                batch_cells, symbol)
-                            for symbol in channel_symbols
-                        }
-                        timer.record_timepoint('Retrieved intensities on chunk')
-                    else:
-                        intensities = None
-
-                    if all([dataset_design.get_feature_name(symbol) in batch_cells.columns
-                            for symbol in channel_symbols]):
-                        feature_names = {symbol: dataset_design.get_feature_name(
-                            symbol) for symbol in channel_symbols}
-                    else:
-                        logger.warning(
-                            'Exact feature names not found in tables. Trying with underscores...')
-                        if all([re.sub(' ', '_', dataset_design.get_feature_name(symbol))
-                                in batch_cells.columns for symbol in channel_symbols]):
-                            feature_names = {symbol: re.sub(' ', '_',
-                                                            dataset_design.get_feature_name(
-                                                                symbol)
-                                                            ) for symbol in channel_symbols}
-                        else:
-                            logger.warning('Not even with underscores.')
-                            missing = [symbol for symbol in channel_symbols
-                                       if not dataset_design.get_feature_name(symbol)
-                                       in batch_cells.columns]
-                            logger.warning('Specifically, the following features not found:')
-                            for symbol in missing:
-                                logger.warning(symbol)
-                            raise ValueError('Could not find feature names in cell manifest.')
-
-                    discretizations = {
-                        symbol: batch_cells[feature_names[symbol]]
-                        for symbol in channel_symbols
-                    }
-                    timer.record_timepoint('Retrieved discretizations on chunk')
-
-                    logger.debug('Starting batch of cells that begins at index %s.', start)
-                    timer.record_timepoint('Started per-cell iteration')
-                    for j, cell in batch_cells.iterrows():
-                        histological_structure_identifier = str(
-                            histological_structure_identifier_index)
-                        histological_structure_identifier_index += 1
-                        shape_file_identifier = str(shape_file_identifier_index)
-                        shape_file_identifier_index += 1
-                        timer.record_timepoint('Beginning of one cell iteration')
-                        shape_file_contents = self.create_shape_file(cell, dataset_design)
-                        timer.record_timepoint('Created shapefile contents')
-                        records['histological_structure'].append((
-                            histological_structure_identifier,
-                            'cell',
-                        ))
-                        records['shape_file'].append((
-                            shape_file_identifier,
-                            'ESRI Shapefile SHP',
-                            shape_file_contents,
-                        ))
-                        records['histological_structure_identification'].append((
-                            histological_structure_identifier,
-                            sha256_hash,
-                            shape_file_identifier,
-                            '\\N',
-                            '',
-                            '',
-                            '',
-                        ))
-                        for symbol in channel_symbols:
-                            target = chemical_species_identifiers_by_symbol[symbol]
-                            if not intensities is None:
-                                quantity = intensities[symbol][j]
-                                if quantity in [None, '']:
-                                    continue
-                                quantity = str(quantity)
-                            else:
-                                quantity = '-1'
-                            discrete_value = discretizations[symbol][j]  # type: ignore
-                            records['expression_quantification'].append((
-                                histological_structure_identifier,
-                                target,
-                                quantity,
-                                '',
-                                '',
-                                'positive' if discrete_value == 1 else 'negative',
-                                '',
-                            ))
-
-                    table_names = [
-                        'histological_structure',
-                        'shape_file',
-                        'histological_structure_identification',
-                        'expression_quantification',
-                    ]
-                    for tablename in table_names:
-                        timer.record_timepoint('Started encoding one chunk')
-                        values_file_contents = '\n'.join([
-                            '\t'.join(r) for r in records[tablename]
-                        ]).encode('utf-8')
-                        timer.record_timepoint('Started inserting chunk into local memory')
-                        with mmap.mmap(-1, len(values_file_contents)) as memmap:
-                            memmap.write(values_file_contents)
-                            memmap.seek(0)
-                            timer.record_timepoint('Started copy from command for bulk insertion')
-                            cursor.copy_from(memmap, tablename)
-                        timer.record_timepoint('Finished inserting one chunk')
-            logger.info('Parsed records for %s cells from "%s".', cells.shape[0], sha256_hash)
+            initial_indices = final_indices
+            final_indices = self.parse_cell_manifest(cursor, filename, channel_symbols,
+                                                     initial_indices)
             timer.record_timepoint('Completed cell manifest parsing')
             logger.debug('Performance report %s:\n%s', file_count,
-                         timer.report_string(organize_by='total time spent'))
+                            timer.report_string(organize_by='total time spent'))
             file_count += 1
             connection.commit()
 

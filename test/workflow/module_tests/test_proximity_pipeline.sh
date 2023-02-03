f5e119fd922734357e2574f568495aa41481a488
James Mathews <mail@jmath.read-books.org>
2023-02-03 17:24:50 -0500

    Using pared down datasets for some tests.

--- test/workflow/module_tests/test_proximity_pipeline.sh
+++ test/workflow/module_tests/test_proximity_pipeline.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-python module_tests/drop_records_for_proximity_testing.py
+FM=../test_data/adi_preprocessed_tables/dataset1/file_manifest.tsv
+cp $FM file_manifest.tsv.bak
+(cat file_manifest.tsv.bak | grep -vE '(0_2|0_3|6_2|6_3|6_4)') > $FM
 
 spt workflow configure --local --input-path=../test_data/adi_preprocessed_tables/dataset1/ --workflow='phenotype proximity' --database-config-file=../db/.spt_db.config.container
 nextflow run .
+
+cp file_manifest.tsv.bak $FM
+rm file_manifest.tsv.bak
 
 status=$?
 rm -f .nextflow.log*; rm -rf .nextflow/; rm -f configure.sh; rm -f run.sh; rm -f main.nf; rm -f nextflow.config; rm -rf work/; rm -rf results/

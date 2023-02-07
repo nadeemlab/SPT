524dec7fcd9d00b70ae9e075bcc2fc1c7d514989
Jimmy Mathews <mathewj2@mskcc.org>
2023-02-07 15:17:28 -0500

    Start fixing cell count for logging in p2p core.

--- spatialprofilingtoolbox/workflow/phenotype_proximity/core.py
+++ spatialprofilingtoolbox/workflow/phenotype_proximity/core.py
@@ -75,10 +75,15 @@ class PhenotypeProximityCoreJob:
             cursor = connection.cursor()
             cursor.execute('''
             SELECT COUNT(*)
-            FROM specimen_collection_process scp
+            FROM
+            histological_structure_identification hsi
+            JOIN histological_structure hs ON hsi.histological_structure=hs.identifier
+            JOIN data_file df ON df.sha256_hash=hsi.data_source
+            JOIN specimen_data_measurement_process sdmp ON df.source_generation_process=sdmp.identifier
+            JOIN specimen_collection_process scp ON scp.specimen=sdmp.specimen
             JOIN study_component sc ON sc.component_study=scp.study
-            WHERE sc.primary_study=%s
-            ''', (self.study_name,))
+            WHERE sc.primary_study=%s AND sdmp.specimen=%s AND hs.anatomical_entity='cell'
+            ''', (self.study_name, self.sample_identifier))
             rows = cursor.fetchall()
             cursor.close()
         return rows[0][0]

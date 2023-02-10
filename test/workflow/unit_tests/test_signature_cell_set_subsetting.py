a8d8d81936688494156ac22a5d1013cc0d85a75d
Jimmy Mathews <mathewj2@mskcc.org>
2023-02-10 12:12:25 -0500

    Fix column names in test.

--- test/workflow/unit_tests/test_signature_cell_set_subsetting.py
+++ test/workflow/unit_tests/test_signature_cell_set_subsetting.py
@@ -15,13 +15,13 @@ if __name__ == '__main__':
     filename = join('..', 'test_data', 'adi_preprocessed_tables/dataset1', '0.csv')
     cells = pd.read_csv(filename, sep=',', keep_default_na=False)
 
-    signature1 = get_pandas_signature(cells, {'CD3': '+', 'B2M': '+'})
+    signature1 = get_pandas_signature(cells, {'CD3_Positive': '+', 'B2M_Positive': '+'})
     computed_sum = sum(1 for entry in signature1 if entry)
     if computed_sum != 27:
         print(f'Got computed sum: {computed_sum}')
         sys.exit(1)
 
-    signature2 = get_pandas_signature(cells, {'PD1': '+', 'FOXP3': '-', 'CD3': '+'})
+    signature2 = get_pandas_signature(cells, {'PD1_Positive': '+', 'FOXP3_Positive': '-', 'CD3_Positive': '+'})
     computed_sum = sum(1 for entry in signature2 if entry)
     if computed_sum != 4:
         print(f'Got computed sum: {computed_sum}')

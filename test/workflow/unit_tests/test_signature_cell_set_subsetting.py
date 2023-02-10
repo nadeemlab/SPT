923dc2250bf4d403180ffe4726c161210f66a9a0
Jimmy Mathews <mathewj2@mskcc.org>
2023-02-10 12:09:43 -0500

    Remove reference to deprecated dataset design function.

--- test/workflow/unit_tests/test_signature_cell_set_subsetting.py
+++ test/workflow/unit_tests/test_signature_cell_set_subsetting.py
@@ -6,24 +6,29 @@ import pandas as pd
 from spatialprofilingtoolbox.workflow.tabular_import.tabular_dataset_design \
     import TabularCellMetadataDesign
 
+def get_pandas_signature(df, signature):
+    return [
+        all(df[key] == 1 if value=='+' else df[key] == 0 for key, value in signature.items())
+        for i, row in df.iterrows()
+    ]
+
+
 if __name__ == '__main__':
     dataset_design = TabularCellMetadataDesign(
         elementary_phenotypes_file=join(
             '..', 'test_data', 'adi_preprocessed_tables/dataset1', 'elementary_phenotypes.csv'),
     )
-    cells = pd.read_csv(join('..', 'test_data', 'adi_preprocessed_tables/dataset1',
-                        '0.csv'), sep=',', keep_default_na=False)
+    filename = join('..', 'test_data', 'adi_preprocessed_tables/dataset1', '0.csv')
+    cells = pd.read_csv(filename, sep=',', keep_default_na=False)
 
-    signature = dataset_design.get_pandas_signature(
-        cells, {'CD3': '+', 'B2M': '+'})
-    computed_sum = sum(1 for entry in signature if entry)
+    signature1 = dataset_design.get_pandas_signature(cells, {'CD3': '+', 'B2M': '+'})
+    computed_sum = sum(1 for entry in signature1 if entry)
     if computed_sum != 27:
         print(f'Got computed sum: {computed_sum}')
         sys.exit(1)
 
-    signature = dataset_design.get_pandas_signature(
-        cells, {'PD1': '+', 'FOXP3': '-', 'CD3': '+'})
-    computed_sum = sum(1 for entry in signature if entry)
+    signature2 = dataset_design.get_pandas_signature(cells, {'PD1': '+', 'FOXP3': '-', 'CD3': '+'})
+    computed_sum = sum(1 for entry in signature2 if entry)
     if computed_sum != 4:
         print(f'Got computed sum: {computed_sum}')
         sys.exit(1)

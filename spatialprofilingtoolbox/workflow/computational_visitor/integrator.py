ccbb1315cfe95092e71b7d224e9d294c39936c46
Jimmy Mathews <mathewj2@mskcc.org>
2023-02-09 22:43:50 -0500

    Depreceate computational visitor abstraction.

--- spatialprofilingtoolbox/workflow/computational_visitor/integrator.py
+++
@@ -1,7 +0,0 @@
-
-    # def skim_final_data(self):
-    #     pass
-    #     # two cohort feature assocation test
-    #     # feature specification
-    #     # feaure specifier
-    #     # diagnostic selection criterion

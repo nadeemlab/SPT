5c5e518c3b2f2d46fefd195f4880f47af40aa7a1
James Mathews <mathewj2@mskcc.org>
2023-03-31 14:57:28 -0400

    Create study scoping for phenotype criteria endpoint and add small test of it. (#140)

---
+++ test/apiserver/module_tests/test_phenotype_criteria.sh
@@ -0,0 +1,29 @@
+
+query="http://spt-apiserver-testing:8080/phenotype-criteria/?study=Melanoma%20intralesional%20IL2&phenotype_symbol=Tumor"
+echo ""
+echo "Query: $query"
+
+curl -s "$query" > /dev/null ;
+if [ "$?" -gt 0 ];
+then
+    echo "Error with apiserver query."
+    curl $query
+    exit 1
+fi
+
+curl -s $query | python -m json.tool > criteria.json
+
+diff module_tests/expected_phenotype_criteria.json criteria.json
+status=$?
+[ $status -eq 0 ] || (echo "API query for named phenotype criteria in the scope of a given study has failed."; )
+
+if [ $status -eq 0 ];
+then
+	rm criteria.json
+    exit 0
+else
+    echo "Response:"
+	cat criteria.json
+	rm criteria.json
+    exit 1
+fi

0426755ff515cf8f5373ad067e3835cbbaf2eef5
James Mathews <mail@jmath.read-books.org>
2023-04-07 15:56:09 -0400

    Start refactoring countsserver to prepare for more handlers.

--- spatialprofilingtoolbox/countsserver/scripts/start.py
+++ spatialprofilingtoolbox/countsserver/scripts/start.py
@@ -13,54 +13,87 @@ class CountsRequestHandler(socketserver.BaseRequestHandler):
     """The TCP server for fast cell counts."""
     def handle(self):
         data = self.request.recv(512).strip()
-        print('Request:')
-        logger.info(data)
-        if data == '':
-            logger.info('Empty body. Serving status.')
-            self.request.sendall((json.dumps(self.server.counts_provider.get_status(
-            ), indent=4) + self.get_end_of_transmission()).encode('utf-8'))
+        logger.info('Request: %s', data)
+        if self.handle_empty_body(data):
             return
+        if self.handle_single_phenotype_counts_request(data):
+            return
+
+    def handle_single_phenotype_counts_request(self, data):
+        groups = self.get_groups(data)
+        if len(groups) != 3:
+            return False
+        specification = self.get_phenotype_spec(groups)
+        study_name = specification[0]
+        if self.handle_missing_study(study_name):
+            return True
+
+        positives_signature, negatives_signature = self.get_signatures(*specification)
+        if self.handle_unparseable_signature(positives_signature, specification[1]):
+            return True
+        if self.handle_unparseable_signature(negatives_signature, specification[2]):
+            return True
+
+        counts = self.get_counts(study_name, positives_signature, negatives_signature)
+        message = json.dumps(counts) + self.get_end_of_transmission()
+        self.request.sendall(message.encode('utf-8'))
+        return True
+
+    def get_counts(self, study, positives_signature, negatives_signature):
+        arguments = [positives_signature, negatives_signature, study]
+        return self.server.counts_provider.count_structures_of_partial_signed_signature(*arguments)
+
+    def handle_unparseable_signature(self, signature, names):
+        if signature is None:
+            logger.error('Could not understand channel names as defining a signature: %s', names)
+            self.wrap_up_transmission()
+            return True
+        return False
+
+    def get_signatures(self, study_name, positives, negatives):
+        signature1 = self.server.counts_provider.compute_signature(positives, study_name)
+        signature2 = self.server.counts_provider.compute_signature(negatives, study_name)
+        logger.info('Signature: %s, %s', signature1, signature2)
+        return signature1, signature2
+
+    def handle_missing_study(self, study_name):
+        if self.server.counts_provider.has_study(study_name):
+            return False
+        logger.error('Study not known to counts server: %s', study_name)
+        self.wrap_up_transmission()
+        return True
+
+    def get_phenotype_spec(self, groups):
         record_separator = chr(30)
-        group_separator = chr(29)
-        groups = data.decode('utf-8').split(group_separator)
         study_name = groups[0]
         positive_channel_names = groups[1].split(record_separator)
         negative_channel_names = groups[2].split(record_separator)
-        if positive_channel_names == ['']:
-            positive_channel_names = []
-        if negative_channel_names == ['']:
-            negative_channel_names = []
+        def trim_empty_entry(element):
+            if element == ['']:
+                return []
+            return element
+        positive_channel_names = trim_empty_entry(positive_channel_names)
+        negative_channel_names = trim_empty_entry(negative_channel_names)
         logger.info('Study: %s', study_name)
         logger.info('Positives: %s', positive_channel_names)
         logger.info('Negatives: %s', negative_channel_names)
-        if not self.server.counts_provider.has_study(study_name):
-            logger.error('Study not known to counts server: %s', study_name)
-            self.request.sendall(''.encode('utf-8'))
-            return
-        positives_signature = self.server.counts_provider.compute_signature(
-            positive_channel_names, study_name)
-        negatives_signature = self.server.counts_provider.compute_signature(
-            negative_channel_names, study_name)
-        logger.info('Signature:')
-        logger.info(positives_signature)
-        logger.info(negatives_signature)
-        if positives_signature is None:
-            logger.error('Could not understand channel names as defining a signature: %s',
-                         positive_channel_names)
-            self.request.sendall(self.get_end_of_transmission().encode('utf-8'))
-            return
-        if negatives_signature is None:
-            logger.error(
-                'Could not understand channel names as defining a signature: %s',
-                negative_channel_names)
-            self.request.sendall(self.get_end_of_transmission().encode('utf-8'))
-            return
-        logger.info('%s', f'{positives_signature:064b}')
-        logger.info('%s', f'{negatives_signature:064b}')
-        counts = self.server.counts_provider.count_structures_of_partial_signed_signature(
-            positives_signature, negatives_signature, study_name)
-        self.request.sendall(
-            (json.dumps(counts) + self.get_end_of_transmission()).encode('utf-8'))
+        return study_name, positive_channel_names, negative_channel_names
+
+    def get_groups(self, data):
+        group_separator = chr(29)
+        return data.decode('utf-8').split(group_separator)
+
+    def handle_empty_body(self, data):
+        if data == '':
+            logger.info('Empty body. Serving status.')
+            status = json.dumps(self.server.counts_provider.get_status(), indent=4)
+            message = status + self.get_end_of_transmission()
+            self.request.sendall(message.encode('utf-8'))
+            return True
+        return False
+
+    def wrap_up_transmission(self):
+        self.request.sendall(self.get_end_of_transmission().encode('utf-8'))
 
     def get_end_of_transmission(self):
         return chr(4)

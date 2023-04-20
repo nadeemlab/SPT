0e32143d17cf07de1b48573004803847d85a0a8c
Jimmy Mathews <mathewj2@mskcc.org>
2023-04-20 13:16:56 -0400

    Add actual test cases for fractions transcription.

--- test/db/module_tests/test_fractions_assessment.py
+++ test/db/module_tests/test_fractions_assessment.py
@@ -1,15 +1,29 @@
 """Test presence of fractions features and assessments of them."""
-import pandas as pd
-
 from spatialprofilingtoolbox.db.database_connection import DatabaseConnectionMaker
 
 if __name__=='__main__':
     with DatabaseConnectionMaker(database_config_file='../db/.spt_db.config.container') as dcm:
         connection = dcm.get_connection()
-        specification = pd.read_sql('SELECT * FROM feature_specification', connection)
-        specifier = pd.read_sql('SELECT * FROM feature_specifier', connection)
-        value_assignment = pd.read_sql('SELECT * FROM quantitative_feature_value', connection)
-
-        print(specification.to_string())
-        print(specifier.to_string())
-        print(value_assignment.to_string())
+        cursor = connection.cursor()
+        cursor.execute('''
+        SELECT
+            fs.specifier,
+            qfv.subject,
+            qfv.value
+        FROM feature_specifier fs
+        JOIN quantitative_feature_value qfv ON qfv.feature=fs.feature_specification
+        ;
+        ''')
+        rows = [(row[0], row[1], float(row[2])) for row in cursor.fetchall()]
+        known_cases = [
+            ('cleaved PARP', 'BaselTMA_SP42_5_X8Y7', 0.31289374212515747),
+            ('CD20', 'lesion 0_3', 0.050000),
+        ]
+        print('Got fractions:')
+        for row in rows:
+            print(row)
+        print('')
+        print('Testing cases:')
+        for case in known_cases:
+            print(case)
+            assert case in rows

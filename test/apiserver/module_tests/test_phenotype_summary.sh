477aea66af0883d4d75bc11d9129a39c7c4ae12e
Jimmy Mathews <mathewj2@mskcc.org>
2023-04-20 12:18:20 -0400

    Include test data artifact.

--- test/apiserver/module_tests/test_phenotype_summary.sh
+++ test/apiserver/module_tests/test_phenotype_summary.sh
@@ -1,7 +1,7 @@
 
 query="http://spt-apiserver-testing:8080/phenotype-summary/?study=Melanoma%20intralesional%20IL2&pvalue=0.1"
 
-curl -s $query ;
+curl -s $query | python -m json.tool | tee summary.json | tr '\n' ' ' | sed 's/ \+//g'
 if [ "$?" -gt 0 ];
 then
     echo "Error with apiserver query."
@@ -9,19 +9,16 @@ then
     exit 1
 fi
 
-# curl -s $query | python -m json.tool > summary.json
+diff module_tests/expected_phenotype_summary.json summary.json
+status=$?
+[ $status -eq 0 ] || (echo "API query for phenotype summary failed."; )
 
-exit 1
-# diff module_tests/expected_phenotype_summary.json summary.json
-# status=$?
-# [ $status -eq 0 ] || (echo "API query for phenotype summary failed."; )
-
-# if [ $status -eq 0 ];
-# then
-# 	rm summary.json
-#     exit 0
-# else
-# 	cat summary.json
-# 	rm summary.json
-#     exit 1
-# fi
+if [ $status -eq 0 ];
+then
+	rm summary.json
+    exit 0
+else
+	cat summary.json
+	rm summary.json
+    exit 1
+fi

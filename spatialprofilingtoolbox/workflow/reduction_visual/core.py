8f5e35421ad9fa25f2cad2e2dfbd6c6bc9c336c5
Jimmy Mathews <mathewj2@mskcc.org>
2023-05-01 11:52:28 -0400

    Separate the "pipeline" part of the umap processing into own class.

--- spatialprofilingtoolbox/workflow/reduction_visual/core.py
+++ spatialprofilingtoolbox/workflow/reduction_visual/core.py
@@ -5,7 +5,7 @@ import warnings
 import pickle
 
 from io import BytesIO
-import base64
+from base64 import b64encode
 
 import pandas as pd
 import numpy as np
@@ -59,11 +59,9 @@ class ReductionVisualCoreJob(CoreJob):
         self.wrap_up_timer()
 
     def generate_and_write_plots(self):
-        RVCJ = ReductionVisualCoreJob
         dense_df = self.retrieve_feature_matrix_dense()
-        normalized = RVCJ.preprocess_univariate_adjustments(dense_df)
-        plot_strings = RVCJ.make_plots(normalized, dense_df)
-        RVCJ.write_table(plot_strings, dense_df)
+        plot_strings = UMAPReducer.create_plots_base64(dense_df)
+        self.write_to_table(plot_strings, dense_df)
 
     def retrieve_feature_matrix_dense(self):
         sparse_df = self.retrieve_feature_matrix_sparse()
@@ -93,6 +91,13 @@ class ReductionVisualCoreJob(CoreJob):
         logger.info('Dataframe pulled: %s', sparse_df.columns.values.tolist())
         return sparse_df
 
+    @staticmethod
+    def sparse_to_dense(sparse_df):
+        logger.info('Converting sparse matrix to dense matrix.')
+        dense_df = sparse_df.pivot(index='structure', columns=['channel'], values=['quantity'])
+        logger.info('Feature matrix created, with columns: %s', dense_df.columns)
+        return dense_df
+
     def validate_all_structures_have_same_targets(self, df):
         if not (df.target.value_counts() == len(df.structure.unique())).all():
             message = 'Cannot create a UMAP representation for study %s because given objects \
@@ -101,103 +106,7 @@ class ReductionVisualCoreJob(CoreJob):
             logger.error(message, self.study_name)
             raise ValueError(message % self.study_name)
 
-    @staticmethod
-    def sparse_to_dense(sparse_df):
-        logger.info('Converting sparse matrix to dense matrix.')
-        dense_df = sparse_df.pivot(index='structure', columns=['channel'], values=['quantity'])
-        logger.info('Feature matrix created, with columns: %s', dense_df.columns)
-        return dense_df
-
-    @staticmethod
-    def preprocess_univariate_adjustments(df):
-        pipe = make_pipeline(SimpleImputer(strategy="mean"), QuantileTransformer())
-        return pipe.fit_transform(df.copy())
-
-    def extract_and_reduce_dimensions(self, df):
-        # logger.info("Converting sparse matrix to dense matrix.")
-        # dense_df = df.pivot(index='structure', columns=['channel'], values=['quantity'])
-        # logger.info("Feature matrix created.")
-
-        # collect vector representations for every structure from (structure-target_quantity) pairs
-        cell_matrix = []
-        cell_classes = []
-        for cell_id in sparse_df.index.unique():
-            cell_df = sparse_df.loc[cell_id]
-            cell_values = cell_df.set_index('target').loc[self.target_order].quantity.values
-            cell_matrix.append(cell_values)
-
-        cell_matrix = np.stack(cell_matrix)
-        logger.info("Feature matrix created")
-
-
-        # Preprocess
-        pipe = make_pipeline(SimpleImputer(strategy="mean"), QuantileTransformer())
-        X = pipe.fit_transform(cell_matrix.copy())
-
-        # Fit UMAP to processed data
-        manifold = UMAP().fit(X)
-        X_reduced = manifold.transform(X)
-
-        logger.info("Finished umap transformation")
-
-        return X_reduced, cell_matrix
-
-    @staticmethod
-    def get_cmap():
-        return LinearSegmentedColormap.from_list('gg', ["gray", "green"], N=256, gamma=1.0)
-
-    @staticmethod
-    def make_plots(X_reduced, dense_df):
-        """ Make scatter plots with color-coded values
-        :param X_reduced: 2d-umap output to use as plot coordinates
-        :param cell_matrix: original target quantity values to use for coloring """
-        plots = []
-        cmap = ReductionVisualCoreJob.get_cmap()
-        for target_idx, target_id in enumerate(self.target_order):
-            f, ax = plt.subplots()  # figsize=(6, 5))
-
-            points = ax.scatter(X_reduced[:, 0], X_reduced[:, 1], c=cell_matrix[:, target_idx], s=5, cmap=cmap,
-                                alpha=0.7)
-            f.colorbar(points)
-
-            # source https://stackoverflow.com/questions/31492525/converting-matplotlib-png-to-base64-for-viewing-in-html-template
-            # write to fictional file and then read the content back from it
-
-            figfile = BytesIO()
-            plt.savefig(figfile, format='svg')
-            figfile.seek(0)  # rewind to beginning of file
-            figdata_svg = base64.b64encode(figfile.getvalue())
-
-            plots.append(figdata_svg)
-            plt.close()
-        return plots
-
-    # def get_mask(self, cells, signature):
-    #     value, multiindex = self.get_value_and_multiindex(signature)
-    #     try:
-    #         loc = cells.set_index(multiindex).index.get_loc(value)
-    #     except KeyError:
-    #         return np.asarray([False,] * cells.shape[0])
-    #     if isinstance(loc, np.ndarray):
-    #         return loc
-    #     if isinstance(loc, slice):
-    #         range1 = [False,]*(loc.start - 0)
-    #         range2 = [True,]*(loc.stop - loc.start)
-    #         range3 = [False,]*(cells.shape[0] - loc.stop)
-    #         return np.asarray(range1 + range2 + range3)
-    #     if isinstance(loc, int):
-    #         return np.asarray([i == loc for i in range(cells.shape[0])])
-    #     raise ValueError(f'Could not select by index: {multiindex}. Got: {loc}')
-    #
-    # def get_value_and_multiindex(self, signature):
-    #     value = (1,) * len(signature['positive']) + (0,) * len(signature['negative'])
-    #     if len(value) == 1:
-    #         value = value[0]
-    #     multiindex = [*signature['positive'], *signature['negative']]
-    #     return value, multiindex
-
-    @staticmethod
-    def write_table(plot_strings, dense_df):
+    def write_to_table(self, plot_strings, dense_df):
         if len(plot_strings) != len(dense_df.columns):
             raise ValueError('Number of computed features not equal to number of cases.')
         df = pd.DataFrame(list(zip(cases, plot_strings)),
@@ -227,3 +136,61 @@ class ReductionVisualCoreJob(CoreJob):
 
     def get_performance_report_filename(self):
         return self.performance_report_file
+
+
+class UMAPReducer:
+    """
+    From dataframe create UMAP-reduce plots in base64 format.
+    """
+    @staticmethod
+    def create_plots_base64(dense_df):
+        normalized = UMAPReducer.preprocess_univariate_adjustments(dense_df)
+        array = UMAPReducer.umap_reduce_to_2d(normalized.to_numpy())
+        return UMAPReducer.make_plots_base64(array, dense_df)
+
+    @staticmethod
+    def preprocess_univariate_adjustments(df):
+        pipeline = make_pipeline(SimpleImputer(strategy="mean"), QuantileTransformer())
+        return pipeline.fit_transform(df.copy())
+
+    @staticmethod
+    def umap_reduce_to_2d(array):
+        manifold = UMAP().fit(array)
+        return manifold.transform(array)
+
+    @staticmethod
+    def get_cmap():
+        return LinearSegmentedColormap.from_list('gg', ["gray", "green"], N=256, gamma=1.0)
+
+    @staticmethod
+    def make_plots_base64(array, dense_df):
+        """
+        Make scatter plots with color-coded values
+
+        :param array: 2D UMAP output in numpy format to use as plot coordinates.
+        :param cell_matrix: Original channel intensity values, pandas DataFrame.
+        """
+        plots_base64 = {}
+        cmap = UMAPReducer.get_cmap()
+        for channel in dense_df.columns:
+            figure, axes = plt.subplots(figsize=(6, 5))
+            points = axes.scatter(
+                array[:, 0],
+                array[:, 1],
+                c=dense_df[channel],
+                s=5,
+                cmap=cmap,
+                alpha=0.7,
+            )
+            figure.colorbar(points)
+            plot_svg_base64 = UMAPReducer.retrieve_base64_from_plot()
+            plots_base64[channel] = plot_svg_base64
+            plt.close()
+        return plots_base64
+
+    @staticmethod
+    def retrieve_base64_from_plot():
+        inmemory_file = BytesIO()
+        plt.savefig(inmemory_file, format='svg')
+        inmemory_file.seek(0)
+        return b64encode(inmemory_file.getvalue())

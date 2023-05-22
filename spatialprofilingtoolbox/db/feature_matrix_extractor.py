3e96c13fad03f8be481de2424901e827f1edc865
James Mathews <mail@jmath.read-books.org>
2023-05-22 16:11:03 -0400

    Add intensity/continuous values dataframe to feature matrix extraction output.

--- spatialprofilingtoolbox/db/feature_matrix_extractor.py
+++ spatialprofilingtoolbox/db/feature_matrix_extractor.py
@@ -22,11 +22,12 @@ class FeatureMatrixExtractor:
     and metadata.
     """
     @staticmethod
-    def extract(database_config_file, specimen: str=None, study: str=None):
+    def extract(database_config_file, specimen: str=None, study: str=None, continuous_also=False):
         E = FeatureMatrixExtractor
         data_arrays = E.retrieve_expressions_from_database(database_config_file,
                                                            specimen=specimen,
-                                                           study=study)
+                                                           study=study,
+                                                           continuous_also=continuous_also)
         centroid_coordinates = E.retrieve_structure_centroids_from_database(database_config_file,
                                                                             specimen=specimen,
                                                                             study=study)
@@ -50,15 +51,18 @@ class FeatureMatrixExtractor:
         for study_name, study in extraction.items():
             for specimen in study['feature matrices'].keys():
                 extraction[study_name]['feature matrices'][specimen]['dataframe'] = None
+                key = 'continuous dataframe'
+                if key in extraction[study_name]['feature matrices'][specimen]:
+                    extraction[study_name]['feature matrices'][specimen][key] = None
             extraction[study_name]['sample cohorts']['assignments'] = None
             extraction[study_name]['sample cohorts']['strata'] = None
 
     @staticmethod
     def retrieve_expressions_from_database(database_config_file, specimen: str=None,
-                                           study: str=None):
+                                           study: str=None, continuous_also=False):
         logger.info('Retrieving expression data from database.')
         with SparseMatrixPuller(database_config_file) as puller:
-            puller.pull(specimen=specimen, study=study)
+            puller.pull(specimen=specimen, study=study, continuous_also=continuous_also)
             data_arrays = puller.get_data_arrays()
         logger.info('Done retrieving expression data from database.')
         return data_arrays.get_studies()
@@ -123,6 +127,19 @@ class FeatureMatrixExtractor:
                     'dataframe': dataframe,
                     'filename': f'{k}.{j}.tsv',
                 }
+
+            if 'continuous data arrays by specimen' in study:
+                specimens = list(study['continuous data arrays by specimen'].keys())
+                for j, specimen in enumerate(sorted(specimens)):
+                    logger.debug('Specimen %s .', specimen)
+                    expression_vectors = study['continuous data arrays by specimen'][specimen]
+                    number_channels = len(study['target index lookup'])
+                    dataframe = pd.DataFrame(
+                        expression_vectors,
+                        columns=[f'F{i}' for i in range(number_channels)],
+                    )
+                    matrices[study_name][specimen]['continuous dataframe'] = dataframe
+
         logger.info('Done creating feature matrices.')
         return matrices
 
--- spatialprofilingtoolbox/workflow/common/sparse_matrix_puller.py
+++ spatialprofilingtoolbox/workflow/common/sparse_matrix_puller.py
@@ -45,6 +45,7 @@ class CompressedDataArrays:
             data_arrays_by_specimen,
             target_index_lookup,
             target_by_symbol,
+            continuous_data_arrays_by_specimen=None,
         ):
         self.check_target_index_lookup(study_name, target_index_lookup)
         self.check_target_by_symbol(study_name, target_by_symbol)
@@ -54,12 +55,27 @@ class CompressedDataArrays:
                 'target index lookup': target_index_lookup,
                 'target by symbol': target_by_symbol,
             }
+            if continuous_data_arrays_by_specimen is not None:
+                key = 'continuous data arrays by specimen'
+                self.studies[study_name][key] = continuous_data_arrays_by_specimen
         else:
-            self.add_more_data_arrays(study_name, data_arrays_by_specimen)
+            self.add_more_data_arrays(
+                study_name,
+                data_arrays_by_specimen,
+                continuous_data_arrays_by_specimen=continuous_data_arrays_by_specimen,
+            )
 
-    def add_more_data_arrays(self, study_name, data_arrays_by_specimen):
+    def add_more_data_arrays(
+            self,
+            study_name,
+            data_arrays_by_specimen,
+            continuous_data_arrays_by_specimen=None,
+        ):
         for key, integers_list in data_arrays_by_specimen.items():
             self.studies[study_name]['data arrays by specimen'][key] = integers_list
+        if continuous_data_arrays_by_specimen is not None:
+            for key, vectors_list in continuous_data_arrays_by_specimen.items():
+                self.studies[study_name]['continuous data arrays by specimen'][key] = vectors_list
 
     def check_target_index_lookup(self, study_name, target_index_lookup):
         if study_name in self.studies:
@@ -87,20 +103,28 @@ class SparseMatrixPuller(DatabaseConnectionMaker):
     def __init__(self, database_config_file):
         super().__init__(database_config_file=database_config_file)
 
-    def pull(self, specimen: str=None, study: str=None):
-        self.data_arrays = self.retrieve_data_arrays(specimen=specimen, study=study)
+    def pull(self, specimen: str=None, study: str=None, continuous_also=False):
+        self.data_arrays = self.retrieve_data_arrays(
+            specimen=specimen, study=study, continuous_also=continuous_also)
 
     def get_data_arrays(self):
         return self.data_arrays
 
-    def retrieve_data_arrays(self, specimen: str=None, study: str=None) -> CompressedDataArrays:
+    def retrieve_data_arrays(self, specimen: str=None, study: str=None, continuous_also=False) -> CompressedDataArrays:
         study_names = self.get_study_names(self.get_connection(), study=study)
         data_arrays = CompressedDataArrays()
         for study_name in study_names:
-            self.fill_data_arrays_for_study(data_arrays, study_name, specimen=specimen)
+            self.fill_data_arrays_for_study(
+                data_arrays, study_name, specimen=specimen, continuous_also=continuous_also)
         return data_arrays
 
-    def fill_data_arrays_for_study(self, data_arrays, study_name, specimen: str=None):
+    def fill_data_arrays_for_study(
+            self,
+            data_arrays,
+            study_name,
+            specimen: str=None,
+            continuous_also=False,
+        ):
         specimens = self.get_pertinent_specimens(study_name, specimen=specimen)
         target_by_symbol = self.get_target_by_symbol(study_name, self.get_connection())
         logger.debug('Pulling sparse entries for study "%s".', study_name)
@@ -118,13 +142,16 @@ class SparseMatrixPuller(DatabaseConnectionMaker):
             )
             if len(sparse_entries) == 0:
                 continue
-            parsed = self.parse_data_arrays_by_specimen(sparse_entries)
-            data_arrays_by_specimen, target_index_lookup = parsed
+            parsed = self.parse_data_arrays_by_specimen(sparse_entries, continuous_also=continuous_also)
+            data_arrays_by_specimen, \
+            target_index_lookup, \
+            continuous_data_arrays_by_specimen = parsed
             data_arrays.add_study_data(
                 study_name,
                 data_arrays_by_specimen,
                 target_index_lookup,
                 target_by_symbol,
+                continuous_data_arrays_by_specimen=continuous_data_arrays_by_specimen,
             )
             progress_reporter.increment(iteration_details=_specimen)
         progress_reporter.done()
@@ -194,7 +221,8 @@ class SparseMatrixPuller(DatabaseConnectionMaker):
         eq.histological_structure,
         eq.target,
         CASE WHEN discrete_value='positive' THEN 1 ELSE 0 END AS coded_value,
-        eq.source_specimen as specimen
+        eq.source_specimen as specimen,
+        eq.quantity as quantity
         FROM expression_quantification eq
         WHERE eq.source_specimen=%s
         ORDER BY eq.source_specimen, eq.histological_structure, eq.target
@@ -207,7 +235,8 @@ class SparseMatrixPuller(DatabaseConnectionMaker):
         eq.histological_structure,
         eq.target,
         CASE WHEN discrete_value='positive' THEN 1 ELSE 0 END AS coded_value,
-        sdmp.specimen as specimen
+        sdmp.specimen as specimen,
+        eq.quantity as quantity
         FROM expression_quantification eq
         JOIN histological_structure hs ON eq.histological_structure=hs.identifier
         JOIN histological_structure_identification hsi ON hs.identifier=hsi.histological_structure
@@ -221,10 +250,11 @@ class SparseMatrixPuller(DatabaseConnectionMaker):
     def get_batch_size(self):
         return 10000000
 
-    def parse_data_arrays_by_specimen(self, sparse_entries):
+    def parse_data_arrays_by_specimen(self, sparse_entries, continuous_also=False):
         target_index_lookup = self.get_target_index_lookup(sparse_entries)
         sparse_entries.sort(key=lambda x: (x[3], x[0]))
         data_arrays_by_specimen = {}
+        continuous_data_arrays_by_specimen = {}
         last_index = len(sparse_entries) - 1
         specimen = sparse_entries[0][3]
         buffer = []
@@ -236,14 +266,23 @@ class SparseMatrixPuller(DatabaseConnectionMaker):
                     cell_count = cell_count + 1
             else:
                 data_arrays_by_specimen[specimen] = [0] * cell_count
-                self.fill_data_array(data_arrays_by_specimen[specimen], buffer, target_index_lookup)
-                logger.debug('Done parsing %s feature vectors from %s.', len(data_arrays_by_specimen[specimen]), specimen)
-                number_mb = int(100 * len(data_arrays_by_specimen[specimen]) * 8 / 1000000) / 100
+                if continuous_also:
+                    continuous_data_arrays_by_specimen[specimen] = [[0]*len(target_index_lookup) for i in range(cell_count)]
+                else:
+                    continuous_data_arrays_by_specimen[specimen] = None
+                self.fill_data_array(
+                    data_arrays_by_specimen[specimen],
+                    buffer,
+                    target_index_lookup,
+                    continuous_data_array = continuous_data_arrays_by_specimen[specimen],
+                )
+                done_message = 'Done parsing %s feature vectors from %s.'
+                logger.debug(done_message, len(data_arrays_by_specimen[specimen]), specimen)
                 if i != last_index:
                     specimen = sparse_entries[i + 1][3]
                     buffer = []
                     cell_count = 1
-        return data_arrays_by_specimen, target_index_lookup
+        return data_arrays_by_specimen, target_index_lookup, continuous_data_arrays_by_specimen
 
     def get_target_index_lookup(self, sparse_entries):
         targets = set([])
@@ -275,7 +314,7 @@ class SparseMatrixPuller(DatabaseConnectionMaker):
         logger.debug('Target by symbol: %s', target_by_symbol)
         return target_by_symbol
 
-    def fill_data_array(self, data_array, entries, target_index_lookup):
+    def fill_data_array(self, data_array, entries, target_index_lookup, continuous_data_array=None):
         structure_index = 0
         for i, entry in enumerate(entries):
             if i > 0:
@@ -284,3 +323,10 @@ class SparseMatrixPuller(DatabaseConnectionMaker):
             if entry[2] == 1:
                 data_array[structure_index] = data_array[structure_index] + \
                     (1 << target_index_lookup[entry[1]])
+        if continuous_data_array is not None:
+            structure_index = 0
+            for i, entry in enumerate(entries):
+                if i > 0:
+                    if entries[i][0] != entries[i-1][0]:
+                        structure_index = structure_index + 1
+                continuous_data_array[structure_index][target_index_lookup[entry[1]]] = float(entry[4])

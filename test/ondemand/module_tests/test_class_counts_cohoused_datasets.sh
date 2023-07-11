3bf88e451c818d7a3f16fa43fcdc34565f572c47
James Mathews <mail@jmath.read-books.org>
2023-07-11 17:48:48 -0400

    Deprecate old test artifacts.

--- test/ondemand/module_tests/test_class_counts_cohoused_datasets.sh
+++ test/ondemand/module_tests/test_class_counts_cohoused_datasets.sh
@@ -15,7 +15,7 @@ consider_exit "$status"
 echo "Class count example in dataset 1 is correct."; echo ''
 
 python module_tests/get_class_counts.py 2 > counts2.txt
-diff counts2.txt module_tests/expected_counts2.json
+diff counts2.txt module_tests/expected_counts_structured2.json
 status="$?"
 cat counts2.txt; echo ''
 rm counts2.txt

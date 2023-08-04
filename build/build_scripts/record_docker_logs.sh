d051abe2987f1c1119ce8a5ad2b84a2b9096b377
James Mathews <mail@jmath.read-books.org>
2023-08-04 19:55:36 -0400

    Small syntax fix.

--- build/build_scripts/record_docker_logs.sh
+++ build/build_scripts/record_docker_logs.sh
@@ -9,5 +9,5 @@ echo -e "${titlecolor}(Started recording Docker compose logs for this test.)${re
 nowtime=$(TZ=GMT date -u +"%Y-%m-%dT%H:%M:%SZ") ; \
 unbuffer docker compose logs --since="$nowtime" -f testing-database > dlogs.db.txt &
 unbuffer docker compose logs --since="$nowtime" -f testing-api-server > dlogs.api.txt &
-unbuffer docker compose logs --since="$nowtime" -f testing-fast-counts-server > dlogs.od.txt&
+unbuffer docker compose logs --since="$nowtime" -f testing-fast-counts-server > dlogs.od.txt &
 tail -f /dev/null

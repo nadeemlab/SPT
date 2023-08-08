e4a019a795b13824ccacdd206b47e476727d9d77
James Mathews <mail@jmath.read-books.org>
2023-08-08 17:03:07 -0400

    Split up counts function.

--- spatialprofilingtoolbox/ondemand/providers/counts_provider.py
+++ spatialprofilingtoolbox/ondemand/providers/counts_provider.py
@@ -1,6 +1,8 @@
 """Count cells for a specific signature, over the specially-created binary-format index."""
 
 from typing import Any
+from typing import Iterable
+from typing import cast
 
 from spatialprofilingtoolbox.ondemand.providers import OnDemandProvider
 from spatialprofilingtoolbox.standalone_utilities.log_formats import colorized_logger
@@ -12,11 +14,38 @@ class CountsProvider(OnDemandProvider):
     """Scan binary-format expression matrices for specific signatures."""
 
     def __init__(self, data_directory: str, load_centroids: bool = False) -> None:
-        """Load from a precomputed JSON artifact in the data directory.
+        """Load from precomputed binary expression files and a JSON index in the data directory.
 
         Note: CountsProvider never loads centroids because it does not need them.
         """
         super().__init__(data_directory, False)
+
+    def count_structures_of_partial_signed_signature(
+        self,
+        positives_signature: int,
+        negatives_signature: int,
+        study_name: str,
+    ) -> dict[str, list[int]]:
+        """Count the number of structures that part of this signature, in signs."""
+        counts = {}
+        for specimen, data_array in self.data_arrays[study_name].items():
+            count = 0
+            if len(data_array) == 0:
+                raise ValueError(f'Data array for specimen "{specimen}" is empty.')
+            count = self.get_count(data_array['integer'], positives_signature, negatives_signature)
+            counts[specimen] = [count, len(data_array)]
+        return counts
+
+    def get_count(self, integers: Iterable, positives_mask: int, negatives_mask: int) -> int:
+        """Counts the number of elements of the list of integer-represented binary numbers which
+        equal to 1 along the bits indicated by the positives mask, and equal to 0 along the bits
+        indicated by the negatives mask.
+        """
+        count = 0
+        for entry in integers:
+            if (entry | positives_mask == entry) and (~entry | negatives_mask == ~entry):
+                count = count + 1
+        return count
 
     def compute_signature(self, channel_names: list[str], study_name: str) -> int | None:
         """Compute int signature of this channel name combination."""
@@ -31,25 +60,6 @@ class CountsProvider(OnDemandProvider):
             signature = signature + (1 << index)
         return signature
 
-    def count_structures_of_partial_signed_signature(
-        self,
-        positives_signature: int,
-        negatives_signature: int,
-        study_name: str,
-    ) -> dict[str, list[int]]:
-        """Count the number of structures that part of this signature, in signs."""
-        counts = {}
-        for specimen, data_array in self.data_arrays[study_name].items():
-            count = 0
-            if len(data_array) == 0:
-                raise ValueError(f'Data array for specimen "{specimen}" is empty.')
-            for entry in data_array['integer']:
-                if (entry | positives_signature == entry) and \
-                        (~entry | negatives_signature == ~entry):
-                    count = count + 1
-            counts[specimen] = [count, len(data_array)]
-        return counts
-
     def get_status(self) -> list[dict[str, Any]]:
         """Get the status of all studies."""
         return [
@@ -59,7 +69,10 @@ class CountsProvider(OnDemandProvider):
                     {
                         'channel symbol': symbol,
                         'count': self.count_structures_of_partial_signed_signature(
-                            [symbol], [], study_name),
+                            cast(int, self.compute_signature([cast(str, symbol)], study_name)),
+                            cast(int, self.compute_signature([], study_name)),
+                            study_name
+                        ),
                     }
                     for symbol in sorted(list(targets['target by symbol'].keys()))
                 ],

57522b8fca550a2f384a0698106ecbda58f88ac9
James Mathews <mail@jmath.read-books.org>
2023-08-14 12:46:55 -0400

    Style conformity in schema infuser script.

--- spatialprofilingtoolbox/db/schema_infuser.py
+++ spatialprofilingtoolbox/db/schema_infuser.py
@@ -1,6 +1,6 @@
 """
-Utility to write the single-cell studies "ADI" SQL schema, plus performance-
-and SPT-related tweaks, into a Postgresql instance.
+Utility to write the single-cell studies "ADI" SQL schema, plus performance- and SPT-related tweaks,
+into a Postgresql instance.
 """
 from importlib.resources import as_file
 from importlib.resources import files
@@ -23,14 +23,18 @@ class SchemaInfuser(DatabaseConnectionMaker):
         super().__init__(database_config_file=database_config_file)
 
     def setup_schema(self, force=False):
-        logger.info(
-            'This creation tool assumes that the database itself and users are already set up.')
-        if force is True:
+        message = 'This creation tool assumes that the database itself and users are already setup.'
+        logger.info(message)
+        if force:
             self.verbose_sql_execute(('drop_views.sql', 'drop views of main schema'))
-            self.verbose_sql_execute((None, 'drop tables from main schema'),
-                                     contents=self.create_drop_tables())
-        self.verbose_sql_execute(('schema.sql', 'create tables from main schema'),
-                                 source_package='adiscstudies')
+            self.verbose_sql_execute(
+                (None, 'drop tables from main schema'),
+                contents=self.create_drop_tables(),
+            )
+        self.verbose_sql_execute(
+            ('schema.sql', 'create tables from main schema'),
+            source_package='adiscstudies',
+        )
         self.verbose_sql_execute(('performance_tweaks.sql', 'tweak main schema'))
         self.verbose_sql_execute(('create_views.sql', 'create views of main schema'))
         self.verbose_sql_execute(('grant_on_tables.sql', 'grant appropriate access to users'))
@@ -55,18 +59,28 @@ class SchemaInfuser(DatabaseConnectionMaker):
         ])
 
     def refresh_views(self):
-        self.verbose_sql_execute(('refresh_views.sql', 'refresh views of main schema'),
-                                  verbosity='itemize')
+        self.verbose_sql_execute(
+            ('refresh_views.sql', 'refresh views of main schema'),
+            verbosity='itemize',
+        )
         transcribe_fraction_features(self)
 
     def recreate_views(self):
         self.verbose_sql_execute(('drop_views.sql', 'drop views of main schema'))
-        self.verbose_sql_execute(('create_views.sql', 'create views of main schema'),
-                                 verbosity='itemize')
+        self.verbose_sql_execute(
+            ('create_views.sql', 'create views of main schema'),
+            verbosity='itemize',
+        )
         self.verbose_sql_execute(('grant_on_tables.sql', 'grant appropriate access to users'))
 
-    def verbose_sql_execute(self, filename_description,
-                            source_package='spatialprofilingtoolbox.db.data_model',
-                            **kwargs):
-        verbose_sql_execute(filename_description, self.get_connection(),
-                            source_package=source_package, **kwargs)
+    def verbose_sql_execute(self,
+        filename_description,
+        source_package='spatialprofilingtoolbox.db.data_model',
+        **kwargs,
+    ):
+        verbose_sql_execute(
+            filename_description,
+            self.get_connection(),
+            source_package=source_package,
+            **kwargs,
+        )

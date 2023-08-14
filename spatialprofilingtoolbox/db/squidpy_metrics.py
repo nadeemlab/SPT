6e0a1568f181daab256dfc3636e6722fd4fb8e97
James Mathews <mail@jmath.read-books.org>
2023-08-14 18:19:48 -0400

    Use fewer instantiations of uploader.

--- spatialprofilingtoolbox/db/squidpy_metrics.py
+++ spatialprofilingtoolbox/db/squidpy_metrics.py
@@ -24,12 +24,13 @@ def _describe_spatial_autocorr_derivation_method() -> str:
         'squidpy.gr.spatial_autocorr for more information.'.lstrip().rstrip()
 
 
-def _spatial_autocorr(data: AnnData, channels: list[str]) -> DataFrame:
+def _spatial_autocorr(data: AnnData) -> DataFrame:
     return spatial_autocorr(
         data,
         attr='X',
         mode='moran',
         corr_method=None,
+        seed=128,
         copy=True,
     )
 
@@ -41,7 +42,10 @@ def create_and_transcribe_squidpy_features(
     """Transcribe "off-demand" Squidpy feature(s) in features system."""
     connection = database_connection_maker.get_connection()
     das = DataAnalysisStudyFactory(connection, study, 'spatial autocorrelation').create()
-    features_by_specimen = _fetch_cells_and_phenotypes(connection.cursor(), study)
+    features_by_specimen, channel_symbols_by_columns_name = _fetch_cells_and_phenotypes(
+        connection.cursor(),
+        study,
+    )
     with ADIFeaturesUploader(
         database_connection_maker,
         data_analysis_study=das,
@@ -50,26 +54,31 @@ def create_and_transcribe_squidpy_features(
         upload_anyway=True,
     ) as feature_uploader:
         for sample, df in features_by_specimen.items():
-            channels = list(set(df.columns).difference(['pixel x', 'pixel y']))
             adata = convert_df_to_anndata(df)
-            autocorr_stats = _spatial_autocorr(adata, channels)
+            autocorr_stats = _spatial_autocorr(adata)
             df_index_to_channel = dict(enumerate(df.columns))
             for df_index_value, row in autocorr_stats.iterrows():
                 channel = str(df_index_to_channel[int(df_index_value)])
                 if channel in {'pixel x', 'pixel y'}:
                     continue
-                feature_uploader.stage_feature_value((channel,), sample, row['pval_norm'])
+                symbol = channel_symbols_by_columns_name[channel]
+                feature_uploader.stage_feature_value((symbol,), sample, row['pval_norm'])
 
 
 def _fetch_cells_and_phenotypes(
     cursor: Psycopg2Cursor,
     study: str,
-) -> dict[str, DataFrame]:
+) -> tuple[dict[str, DataFrame], dict[str, str]]:
     extractor = FeatureMatrixExtractor(cursor)
     bundle = cast(Bundle, extractor.extract(study=study))
     FeatureMatrices = dict[str, dict[str, DataFrame | str]]
     feature_matrices = cast(FeatureMatrices, bundle[study]['feature matrices'])
-    return {
+    features_by_specimen = {
         specimen: cast(DataFrame, packet['dataframe'])
         for specimen, packet in feature_matrices.items()
     }
+    channel_symbols_by_columns_name = cast(
+        dict[str, str],
+        bundle[study]['channel symbols by column name'],
+    )
+    return features_by_specimen, channel_symbols_by_columns_name

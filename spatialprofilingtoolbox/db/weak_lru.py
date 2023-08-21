cbdb539b7bfdde7b31e20847e8e45221f418bbcf
Jimmy Mathews <mathewj2@mskcc.org>
2023-08-21 00:07:11 -0400

    Fix self reference to weak.

--- spatialprofilingtoolbox/db/weak_lru.py
+++ spatialprofilingtoolbox/db/weak_lru.py
@@ -3,13 +3,16 @@
 import functools
 import weakref
 
-def weak_lru(maxsize=128, typed=False):
-    def wrapper(func):
-        @functools.lru_cache(maxsize, typed)
-        def _func(_self, *args, **kwargs):
-            return func(_self(), *args, **kwargs)
+def weak_lru(*lru_args, **lru_kwargs):
+    def decorator(func):
         @functools.wraps(func)
-        def inner(self, *args, **kwargs):
-            return _func(weakref.ref(self), *args, **kwargs)
-        return inner
-    return wrapper
+        def wrapped_func(self, *args, **kwargs):
+            self_weak = weakref.ref(self)
+            @functools.wraps(func)
+            @functools.lru_cache(*lru_args, **lru_kwargs)
+            def cached_method(*args, **kwargs):
+                return func(self_weak(), *args, **kwargs)
+            setattr(self, func.__name__, cached_method)
+            return cached_method(*args, **kwargs)
+        return wrapped_func
+    return decorator

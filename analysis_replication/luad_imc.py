37811520450d1d95cdc9a93eef0ab0d92b4f41f1
James Mathews <mail@jmath.read-books.org>
2023-08-24 16:02:22 -0400

    Update sorin reanalysis.

--- analysis_replication/luad_imc.py
+++ analysis_replication/luad_imc.py
@@ -22,7 +22,6 @@ mean1 = mean(values1)
 mean2 = mean(values2)
 print((mean2, mean1, mean2 / mean1))
 
-
 df = access.neighborhood_enrichment(['CD163+ macrophage', 'Regulatory T cell'])
 values1 = df[df['cohort'] == '1']['CD163+ macrophage and Regulatory T cell']
 values2 = df[df['cohort'] == '2']['CD163+ macrophage and Regulatory T cell']
@@ -30,38 +29,29 @@ mean1 = mean(values1)
 mean2 = mean(values2)
 print((mean2, mean1, mean2 / mean1))
 
+df = access.neighborhood_enrichment(['CD163+ macrophage', 'Endothelial cell'])
+values1 = df[df['cohort'] == '1']['CD163+ macrophage and Regulatory T cell']
+values2 = df[df['cohort'] == '2']['CD163+ macrophage and Regulatory T cell']
+mean1 = mean(values1)
+mean2 = mean(values2)
+print((mean2, mean1, mean2 / mean1))
 
-# df = access.proximity([KRT['14'], KRT['7']])
-# values1 = df[df['cohort'] == '1']['proximity, KRT14+ CK+ and KRT7+ CK+']
-# values2 = df[df['cohort'] == '2']['proximity, KRT14+ CK+ and KRT7+ CK+']
-# mean1 = mean(values1)
-# mean2 = mean(values2)
-# print((mean2, mean1, mean2 / mean1))
+klrd1 = {'positive_markers': ['KLRD1'], 'negative_markers': []}
+cd14 = {'positive_markers': ['CD14'], 'negative_markers': []}
+cd14_fcgr3a = {'positive_markers': ['CD14', 'FCGR3A'], 'negative_markers': []}
+df = access.counts([klrd1, cd14, cd14_fcgr3a])
+fractions = df['KLRD1+'] / df['CD14+ FCGR3A+']
+fractions = fractions[fractions != inf]
+fractions1 = fractions[df['cohort'] == '1']
+fractions2 = fractions[df['cohort'] == '2']
+mean1 = mean(fractions1)
+mean2 = mean(fractions2)
+print((mean2, mean1, mean2 / mean1))
 
-# df = access.proximity([KRT['14'], KRT['5']])
-# values2 = df[df['cohort'] == '2']['proximity, KRT14+ CK+ and KRT5+ CK+']
-# values3 = df[df['cohort'] == '3']['proximity, KRT14+ CK+ and KRT5+ CK+']
-# mean2 = mean(values2)
-# mean3 = mean(values3)
-# print((mean2, mean3, mean2 / mean3))
-
-# df = access.counts([KRT['14'], KRT['7']])
-# fractions = df['KRT14+ CK+ and KRT7+ CK+'] / df['all cells']
-# print(mean(fractions))
-
-
-# df = access.counts([KRT['14'], KRT['5']])
-# fractions = df['KRT14+ CK+ and KRT5+ CK+'] / df['all cells']
-# print(mean(fractions))
-
-# df = access.counts([KRT['14'], KRT['19']])
-# fractions = df['KRT14+ CK+'] / df['KRT19+ CK+']
-# fractions = fractions[fractions != inf]
-# fractions1 = fractions[df['cohort'] == '1']
-# fractions2 = fractions[df['cohort'] == '2']
-# fractions3 = fractions[df['cohort'] == '3']
-# mean1 = mean(fractions1)
-# mean2 = mean(fractions2)
-# mean3 = mean(fractions3)
-# print((mean2, mean3, mean2 / mean3))
-# print((mean2, mean1, mean2 / mean1))
+fractions = df['KLRD1+'] / df['CD14+']
+fractions = fractions[fractions != inf]
+fractions1 = fractions[df['cohort'] == '1']
+fractions2 = fractions[df['cohort'] == '2']
+mean1 = mean(fractions1)
+mean2 = mean(fractions2)
+print((mean2, mean1, mean2 / mean1))

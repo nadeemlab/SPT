876f55e0d9eb6b9d371b1f31c9af045ba5536b7b
James Mathews <mail@jmath.read-books.org>
2023-08-24 16:09:47 -0400

    Fix name typo

--- analysis_replication/luad_imc.py
+++ analysis_replication/luad_imc.py
@@ -15,23 +15,22 @@ else:
 
 
 df = access.spatial_autocorrelation('B cell')
-print(df)
-values1 = df[df['cohort'] == '1']['B cell']
-values2 = df[df['cohort'] == '2']['B cell']
+values1 = df[df['cohort'] == '1']['spatial autocorrelation, B cell']
+values2 = df[df['cohort'] == '2']['spatial autocorrelation, B cell']
 mean1 = mean(values1)
 mean2 = mean(values2)
 print((mean2, mean1, mean2 / mean1))
 
 df = access.neighborhood_enrichment(['CD163+ macrophage', 'Regulatory T cell'])
-values1 = df[df['cohort'] == '1']['CD163+ macrophage and Regulatory T cell']
-values2 = df[df['cohort'] == '2']['CD163+ macrophage and Regulatory T cell']
+values1 = df[df['cohort'] == '1']['neighborhood enrichment, CD163+ macrophage and Regulatory T cell']
+values2 = df[df['cohort'] == '2']['neighborhood enrichment, CD163+ macrophage and Regulatory T cell']
 mean1 = mean(values1)
 mean2 = mean(values2)
 print((mean2, mean1, mean2 / mean1))
 
 df = access.neighborhood_enrichment(['CD163+ macrophage', 'Endothelial cell'])
-values1 = df[df['cohort'] == '1']['CD163+ macrophage and Regulatory T cell']
-values2 = df[df['cohort'] == '2']['CD163+ macrophage and Regulatory T cell']
+values1 = df[df['cohort'] == '1']['neighborhood enrichment, CD163+ macrophage and Endothelial cell']
+values2 = df[df['cohort'] == '2']['neighborhood enrichment, CD163+ macrophage and Endothelial cell']
 mean1 = mean(values1)
 mean2 = mean(values2)
 print((mean2, mean1, mean2 / mean1))
@@ -39,6 +38,7 @@ print((mean2, mean1, mean2 / mean1))
 klrd1 = {'positive_markers': ['KLRD1'], 'negative_markers': []}
 cd14 = {'positive_markers': ['CD14'], 'negative_markers': []}
 cd14_fcgr3a = {'positive_markers': ['CD14', 'FCGR3A'], 'negative_markers': []}
+
 df = access.counts([klrd1, cd14, cd14_fcgr3a])
 fractions = df['KLRD1+'] / df['CD14+ FCGR3A+']
 fractions = fractions[fractions != inf]

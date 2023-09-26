8cd88d1b585190636560537f56fa28e5d1589f91
James Mathews <mail@jmath.read-books.org>
2023-09-26 12:52:59 -0400

    Expand scope of test somewhat.

--- test/workflow/unit_tests/test_feature_matrix_extraction.py
+++ test/workflow/unit_tests/test_feature_matrix_extraction.py
@@ -131,7 +131,7 @@ if __name__ == '__main__':
 
     some_histological_structures = extractor.extract(
         study=study_name,
-        histological_structures={1, 5, 150, 151, 2000},  # 2000 is OOB but should skip, not fail
+        histological_structures={1, 5, 7, 15, 16, 17, 150, 151, 340, 341, 2000},  # 2000 is OOB but should skip, not fail
         retain_structure_id=True,
     )
     test_sample_set(some_histological_structures)

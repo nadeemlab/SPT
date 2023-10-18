ff5e75602db3c7e229ce141a17a9c59adf0c3492
James Mathews <mail@jmath.read-books.org>
2023-10-18 18:04:59 -0400

    Remove duplicate statement

--- spatialprofilingtoolbox/workflow/tabular_import/parsing/skimmer.py
+++ spatialprofilingtoolbox/workflow/tabular_import/parsing/skimmer.py
@@ -115,9 +115,6 @@ class DataSkimmer:
         finally:
             connection.close()
 
-        with DBCursor(database_config_file=self.database_config_file) as cursor:
-            cursor.execute(create_statement)
-
     def _register_study_database_name(self, study_name: str, database_name: str) -> None:
         with DBCursor(database_config_file=self.database_config_file) as cursor:
             cursor.execute('INSERT VALUES (%s, %s) INTO study_lookup;', (study_name, database_name))

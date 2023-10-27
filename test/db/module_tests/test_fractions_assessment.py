00b7098b96fe472d552a7d28a265dc3611a32a6d
Jimmy Mathews <mathewj2@mskcc.org>
2023-10-27 01:57:02 -0400

    Update fractions test to select only fractions.

--- test/db/module_tests/test_fractions_assessment.py
+++ test/db/module_tests/test_fractions_assessment.py
@@ -3,6 +3,7 @@
 from pandas import read_csv
 
 from spatialprofilingtoolbox.db.database_connection import DBCursor
+from spatialprofilingtoolbox import get_feature_description
 
 def retrieve_known_cases():
     df = read_csv('module_tests/expected_fractions.tsv', sep='\t')
@@ -24,19 +25,22 @@ if __name__=='__main__':
                 qfv.value
             FROM feature_specifier fs
             JOIN quantitative_feature_value qfv ON qfv.feature=fs.feature_specification
+            JOIN feature_specification fsp ON fsp.identifier=fs.feature_specification
+            WHERE fsp.derivation_method=%s
             ;
-            ''')
+            ''', (get_feature_description('population fractions'),))
             rows.extend([(row[0], row[1], round6(float(row[2]))) for row in cursor.fetchall()])
+
     known_cases = retrieve_known_cases()
-    print('Got fractions:')
-    for i, row in enumerate(rows):
+    print(f'Got fractions ({len(rows)}):')
+    for i, row in enumerate(sorted(rows)):
         print(row)
         if i > 25:
             print('...')
             break
     print('')
     print(f'Testing {len(known_cases)} cases.')
-    for i, case in enumerate(known_cases):
+    for i, case in enumerate(sorted(known_cases)):
         if i < 25:
             print(case)
         if i == 26:

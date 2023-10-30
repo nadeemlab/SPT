fe965d8c61d0dfb4a3e6a3d68626fa50a08d8a55
James Mathews <mail@jmath.read-books.org>
2023-10-30 13:16:30 -0400

    Fix floating point arithmetic in test.

--- test/workflow/module_tests/test_cggnn.sh
+++ test/workflow/module_tests/test_cggnn.sh
@@ -7,13 +7,11 @@ status="$?"
 
 model_size=$(wc -c <"results/mi2_model.pt")
 canon_model_size=1362921
-if [[ $model_size < $(( .99 * $canon_model_size )) || $model_size > $(( 1.01 * $canon_model_size )) ]];
+if (( (100 * $model_size < $(( 99 * $canon_model_size ))) || (100 * $model_size > $(( 101 * $canon_model_size ))) ));
 then
     echo "Output model is not within 1% of expected size ($canon_model_size): $model_size"
     exit 1
 fi
-
-head "results/mi2_importances.csv"
 
 importance_length=$(wc -l <"results/mi2_importances.csv")
 canon_importance_length=180
@@ -22,8 +20,6 @@ then
     echo "Output importance file ($importance_length) is not the expected length ($canon_importance_length)."
     exit 1
 fi
-
-cut -f2 -d, mi2_importances.csv  | sort | head | grep -o '[0-9]\.[0-9]\{0,4\}'
 
 rm -f .nextflow.log*; rm -rf .nextflow/; rm -f configure.sh; rm -f run.sh; rm -f main.nf; rm -f nextflow.config; rm -rf results/
 

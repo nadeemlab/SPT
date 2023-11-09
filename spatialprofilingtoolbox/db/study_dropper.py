f3387810b5841bc28fd71cf4a7fa4f4c0eeefba9
James Mathews <mail@jmath.read-books.org>
2023-11-09 18:02:20 -0500

    Deal with transaction.

--- spatialprofilingtoolbox/db/study_dropper.py
+++ spatialprofilingtoolbox/db/study_dropper.py
@@ -1,6 +1,7 @@
 """Drop a single study."""
 
 from spatialprofilingtoolbox.db.database_connection import DBCursor
+from spatialprofilingtoolbox.db.database_connection import DBConnection
 from spatialprofilingtoolbox.standalone_utilities.log_formats import colorized_logger
 
 logger = colorized_logger(__name__)
@@ -20,8 +21,10 @@ class StudyDropper:
                 return
             database_name = rows[0][0]
 
-        with DBCursor(database_config_file=database_config_file, autocommit = False) as cursor:
-            cursor.execute('DROP DATABASE %s' % database_name)
+        with DBConnection(database_config_file=database_config_file, autocommit = False) as connection:
+            connection.set_session(autocommit = True)
+            with connection.cursor() as cursor:
+                cursor.execute('DROP DATABASE %s ;' % database_name)
 
         with DBCursor(database_config_file=database_config_file) as cursor:
             cursor.execute('DELETE FROM study_lookup WHERE study=%s ;', (study,))

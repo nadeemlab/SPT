87329256985fd3ced9013a77853f59d5fe579079
James Mathews <mail@jmath.read-books.org>
2023-11-20 17:57:55 -0500

    Revert inclusion

--- spatialprofilingtoolbox/cggnn/cggnn/README.md
+++
@@ -1,1 +0,0 @@
-A fork of [cg-gnn](https://github.com/CarlinLiao/cg-gnn/), itself a fork from several additional libraries.
--- spatialprofilingtoolbox/cggnn/cggnn/util/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Utility functions for the CG-GNN pipeline."""
-
-from spatialprofilingtoolbox.cggnn.cggnn.util.ml import CellGraphModel
-from spatialprofilingtoolbox.cggnn.cggnn.util.util import instantiate_model, set_seeds

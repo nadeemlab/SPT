5ba654a7a052307b6b273f8769dc95d7e230f916
James Mathews <mail@jmath.read-books.org>
2023-12-01 17:26:33 -0500

    Fix labels used and comment out insignificant results.

--- analysis_replication/urothelial_ici.py
+++ analysis_replication/urothelial_ici.py
@@ -13,67 +13,75 @@ def test(host):
     lag3 = {'positive_markers': ['LAG3'], 'negative_markers': []}
     df = access.counts([cd3tumoral, lag3])
 
-    fractions = df['CD3+ intratumoral+ and LAG3+'] / df['all cells']
-    fractions1 = fractions[df['cohort'] == '1']
-    fractions3 = fractions[df['cohort'] == '3']
-    compare(fractions1, fractions3, expected_fold=0.0857, show_pvalue=True)
+    # fractions = df['CD3+ intratumoral+ and LAG3+'] / df['all cells']
+    # fractions1 = fractions[df['cohort'] == '1']
+    # fractions2 = fractions[df['cohort'] == '2']
+    # compare(fractions2, fractions1, expected_fold=2.454, show_pvalue=True)
 
-    fractions = df['CD3+ intratumoral+ and LAG3+'] / df['CD3+ intratumoral+']
-    fractions1 = fractions[df['cohort'] == '1']
-    fractions3 = fractions[df['cohort'] == '3']
-    compare(fractions1, fractions3, expected_fold=0.423, show_pvalue=True)
+    # fractions = df['CD3+ intratumoral+ and LAG3+'] / df['CD3+ intratumoral+']
+    # fractions1 = fractions[df['cohort'] == '1']
+    # fractions2 = fractions[df['cohort'] == '2']
+    # compare(fractions2, fractions1, expected_fold=1.546, show_pvalue=True)
 
     cd3lag3 = {'positive_markers': ['CD3', 'LAG3'], 'negative_markers': []}
     cd8ki67 = {'positive_markers': ['CD8', 'KI67'], 'negative_markers': []}
     cd8ki67tumoral = {'positive_markers': ['CD8', 'KI67', 'intratumoral'], 'negative_markers': []}
+    cd8tumoral = {'positive_markers': ['CD8', 'intratumoral'], 'negative_markers': []}
     cd8pd1lag3 = {'positive_markers': ['CD8', 'PD1', 'LAG3'], 'negative_markers': []}
     cd3pd1lag3 = {'positive_markers': ['CD3', 'PD1', 'LAG3'], 'negative_markers': []}
     cd3pd1lag3stromal = {'positive_markers': ['CD3', 'PD1', 'LAG3', 'stromal'], 'negative_markers': []}
     cd3stromal = {'positive_markers': ['CD3', 'stromal'], 'negative_markers': []}
+    tumor = {'positive_markers': ['PanCK-SOX10'], 'negative_markers': []}
     tumoral = {'positive_markers': ['intratumoral'], 'negative_markers': []}
     stromal = {'positive_markers': ['stromal'], 'negative_markers': []}
 
     df = access.counts([cd8ki67, tumoral])
     fractions = df['CD8+ KI67+ and intratumoral+'] / df['all cells']
     fractions1 = fractions[df['cohort'] == '1']
-    fractions3 = fractions[df['cohort'] == '3']
-    compare(fractions1, fractions3, expected_fold=0.076, show_pvalue=True)
+    fractions2 = fractions[df['cohort'] == '2']
+    compare(fractions2, fractions1, expected_fold=3.520, show_pvalue=True)
 
-    df = access.counts([cd8ki67tumoral, 'Tumor'])
-    fractions = df['CD8+ KI67+ intratumoral+'] / df['Tumor']
+    df = access.counts([cd8ki67tumoral, tumor])
+    fractions = df['CD8+ KI67+ intratumoral+'] / df['PanCK-SOX10+']
     fractions1 = fractions[df['cohort'] == '1']
-    fractions3 = fractions[df['cohort'] == '3']
-    compare(fractions1, fractions3, expected_fold=0.1, show_pvalue=True)
+    fractions2 = fractions[df['cohort'] == '2']
+    compare(fractions2, fractions1, expected_fold=6.508, show_pvalue=True)
+
+    df = access.counts([cd8tumoral, tumor])
+    fractions = df['CD8+ intratumoral+'] / df['PanCK-SOX10+']
+    fractions1 = fractions[df['cohort'] == '1']
+    fractions2 = fractions[df['cohort'] == '2']
+    compare(fractions2, fractions1, expected_fold=3.113, show_pvalue=True)
 
     df = access.counts([cd8pd1lag3, tumoral])
     fractions = df['CD8+ PD1+ LAG3+ and intratumoral+'] / df['all cells']
     fractions1 = fractions[df['cohort'] == '1']
-    fractions3 = fractions[df['cohort'] == '3']
-    compare(fractions1, fractions3, expected_fold=0.061, show_pvalue=True)
+    fractions2 = fractions[df['cohort'] == '2']
+    compare(fractions2, fractions1, expected_fold=3.095, show_pvalue=True)
 
-    df = access.counts([cd8pd1lag3, stromal])
-    fractions = df['CD8+ PD1+ LAG3+ and stromal+'] / df['all cells']
-    fractions1 = fractions[df['cohort'] == '1']
-    fractions3 = fractions[df['cohort'] == '3']
-    compare(fractions1, fractions3, expected_fold=0.074, show_pvalue=True)
+    # df = access.counts([cd8pd1lag3, stromal])
+    # fractions = df['CD8+ PD1+ LAG3+ and stromal+'] / df['all cells']
+    # fractions1 = fractions[df['cohort'] == '1']
+    # fractions2 = fractions[df['cohort'] == '2']
+    # compare(fractions2, fractions1, expected_fold=1.063, show_pvalue=True)
 
-    df = access.counts([cd3pd1lag3, tumoral])
-    fractions = df['CD3+ PD1+ LAG3+ and intratumoral+'] / df['all cells']
-    fractions1 = fractions[df['cohort'] == '1']
-    fractions3 = fractions[df['cohort'] == '3']
-    compare(fractions1, fractions3, expected_fold=0.063, show_pvalue=True)
+    # df = access.counts([cd3pd1lag3, tumoral])
+    # fractions = df['CD3+ PD1+ LAG3+ and intratumoral+'] / df['all cells']
+    # fractions1 = fractions[df['cohort'] == '1']
+    # fractions2 = fractions[df['cohort'] == '2']
+    # compare(fractions2, fractions1, expected_fold=2.151, show_pvalue=True)
 
-    df = access.counts([cd3pd1lag3, stromal])
-    fractions = df['CD3+ PD1+ LAG3+ and stromal+'] / df['all cells']
-    fractions1 = fractions[df['cohort'] == '1']
-    fractions3 = fractions[df['cohort'] == '3']
-    compare(fractions1, fractions3, expected_fold=0.081, show_pvalue=True)
+    # df = access.counts([cd3pd1lag3, stromal])
+    # fractions = df['CD3+ PD1+ LAG3+ and stromal+'] / df['all cells']
+    # fractions1 = fractions[df['cohort'] == '1']
+    # fractions2 = fractions[df['cohort'] == '2']
+    # compare(fractions2, fractions1, expected_fold=1.535, show_pvalue=True)
 
     df = access.counts([cd3pd1lag3stromal, cd3stromal])
     fractions = df['CD3+ PD1+ LAG3+ stromal+'] / df['CD3+ stromal+']
     fractions1 = fractions[df['cohort'] == '1']
-    fractions3 = fractions[df['cohort'] == '3']
-    compare(fractions1, fractions3, expected_fold=0.144, show_pvalue=True)
+    fractions2 = fractions[df['cohort'] == '2']
+    compare(fractions2, fractions1, expected_fold=2.921, show_pvalue=True)
 
 if __name__=='__main__':
     host: str | None

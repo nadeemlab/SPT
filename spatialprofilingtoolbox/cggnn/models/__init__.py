a99ef7d31419156b12eed0d3cdc31a91f36156cb
Carlin Liao <carlin.liao@berkeley.edu>
2023-12-06 17:19:05 -0500

    abstracted model addition

--- spatialprofilingtoolbox/cggnn/models/__init__.py
+++ spatialprofilingtoolbox/cggnn/models/__init__.py
@@ -6,17 +6,23 @@ from os.path import abspath, dirname, join
 from importlib.abc import MetaPathFinder
 
 
-class _CG_GNNImportHook(MetaPathFinder):
-    """An import hook to detect if cg_gnn is trying to be imported."""
+class _CGModelImportHook(MetaPathFinder):
+    """An import hook for if one of these externally manageed models is imported."""
 
     def find_spec(self, fullname, path, target=None):
-        """Add cg_gnn to the Python path when something tries to import it."""
-        if fullname == f'{__name__}.cg_gnn':
-            current_dir = dirname(abspath(__file__))
-            cggnn_dir = join(current_dir, 'cg_gnn')
-            if cggnn_dir not in sys_path:
-                sys_path.insert(0, cggnn_dir)
+        """Add the model to the Python path when something tries to import it."""
+        for model in {'cg_gnn'}:
+            _maybe_add_module_to_path(fullname, model)
         return None
 
 
-meta_path.insert(0, _CG_GNNImportHook())
+def _maybe_add_module_to_path(fullname: str, model: str):
+    """Add this module to the Python path if it's being imported."""
+    if fullname == f'{__name__}.{model}':
+        current_dir = dirname(abspath(__file__))
+        model_dir = join(current_dir, model)
+        if model_dir not in sys_path:
+            sys_path.insert(0, model_dir)
+
+
+meta_path.insert(0, _CGModelImportHook())

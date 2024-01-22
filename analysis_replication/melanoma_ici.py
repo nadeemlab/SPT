4397216891ff72eadeef2e38528d2c60b58bb118
James Mathews <mail@jmath.read-books.org>
2024-01-22 17:46:43 -0500

    Add more analysis records

--- analysis_replication/melanoma_ici.py
+++ analysis_replication/melanoma_ici.py
@@ -14,8 +14,9 @@ def test(host):
     antigen_experienced_cytotoxic = {'positive_markers': ['CD8A', 'CD3', 'CD45RO'], 'negative_markers': []}
     antigen_experienced_cytotoxic_min = {'positive_markers': ['CD8A', 'CD45RO'], 'negative_markers': []}
 
-    # # The average value of the neighborhood enrichment score for phenotype(s) CD3+ CD45RO+ CD8A+ and
-    # # Melanoma is ... times higher in cohort 1 than in cohort 2.
+    # Poor p values
+    # # # The average value of the neighborhood enrichment score for phenotype(s) CD3+ CD45RO+ CD8A+ and
+    # # # Melanoma is ... times higher in cohort 1 than in cohort 2.
     # df = access.neighborhood_enrichment([antigen_experienced_cytotoxic, 'Melanoma'])
     # values1 = df[df['cohort'] == '1']['neighborhood enrichment, CD8A+ CD3+ CD45RO+ and Melanoma']
     # values2 = df[df['cohort'] == '2']['neighborhood enrichment, CD8A+ CD3+ CD45RO+ and Melanoma']
@@ -107,16 +108,95 @@ def test(host):
     # fractions2 = fractions[df['cohort'] == '2']
     # compare(fractions2, fractions1, expected_fold=1.69, show_pvalue=True)
 
-    df = access.counts(['Endothelial stroma', 'Melanoma'])
-    fractions = df['Endothelial stroma'] / df['Melanoma']
-    fractions1 = fractions[df['cohort'] == '1']
-    fractions2 = fractions[df['cohort'] == '2']
-    compare(fractions2, fractions1, expected_fold=7.44, show_pvalue=True)
+    # df = access.counts(['Endothelial stroma', 'Melanoma'])
+    # fractions = df['Endothelial stroma'] / df['Melanoma']
+    # fractions1 = fractions[df['cohort'] == '1']
+    # fractions2 = fractions[df['cohort'] == '2']
+    # compare(fractions2, fractions1, expected_fold=7.44, show_pvalue=True)
 
-    fractions = df['Melanoma'] / df['Endothelial stroma']
-    fractions1 = fractions[df['cohort'] == '1']
-    fractions2 = fractions[df['cohort'] == '2']
-    compare(fractions2, fractions1, expected_fold=0.101, show_pvalue=True)
+    # fractions = df['Melanoma'] / df['Endothelial stroma']
+    # fractions1 = fractions[df['cohort'] == '1']
+    # fractions2 = fractions[df['cohort'] == '2']
+    # compare(fractions2, fractions1, expected_fold=0.101, show_pvalue=True)
+
+
+    # Middling p
+    # On average, the ratio of the number of cells that are Lineage including macrophage and B cellss to those that are Naive cytotoxic T cells is 3.21 times higher in cohort 2 than in cohort 1.
+    # df = access.counts(['Lineage including macrophage', 'B cells'])
+    # df2 = access.counts(['Naive cytotoxic T cell', 'B cells'])
+    # fractions = df['Lineage including macrophage and B cells'] / df2['Naive cytotoxic T cell']
+    # fractions1 = fractions[df['cohort'] == '1']
+    # fractions2 = fractions[df['cohort'] == '2']
+    # compare(fractions2, fractions1, expected_fold=0.280, show_pvalue=True)  # p=0.12
+
+    # Poor p:
+
+    # On average, the ratio of the number of cells that are Lineage including monocyte and B cellss to those that are Naive cytotoxic T cells is 3.73 times higher in cohort 2 than in cohort 1.
+    # ...
+
+    # On average, the ratio of the number of cells that are Naive T helper cells and B cells to those that are Naive cytotoxic T cells is 4.08 times higher in cohort 2 than in cohort 1.
+    # ...
+
+    # On average, the ratio of the number of cells that are Naive T helper cells and B cells to those that are T regulatory cellss is 34.96 times higher in cohort 2 than in cohort 1.
+    # ...
+
+    # On average, the ratio of the number of cells that are T helper cells antigen-experienced and B cells to those that are Naive cytotoxic T cells is 3.44 times higher in cohort 2 than in cohort 1.
+    # ...
+
+    # On average, the ratio of the number of cells that are T helper cells antigen-experienced and B cells to those that are T regulatory cellss is 24.75 times higher in cohort 2 than in cohort 1.
+    # ...
+
+    # On average, the ratio of the number of cells that are Naive cytotoxic T cells and Cytotoxic T cell antigen-experienced to those that are Lineage including monocyte is 201.55 times higher in cohort 1 than in cohort 2.
+    # ...
+
+    # On average, the ratio of the number of cells that are Naive cytotoxic T cells and Cytotoxic T cell antigen-experienced to those that are Lineage including macrophage is 269.05 times higher in cohort 1 than in cohort 2.
+    # ...
+
+    # On average, the ratio of the number of cells that are Lineage including monocyte and Lineage including macrophage to those that are B cellss is 31.80 times higher in cohort 1 than in cohort 2.
+    # ...
+
+    # On average, the ratio of the number of cells that are T helper cells antigen-experienced and Naive T helper cell to those that are T regulatory cellss is 3.95 times higher in cohort 2 than in cohort 1.
+    # ...
+
+    # On average, the ratio of the number of cells that are T helper cells antigen-experienced and Naive T helper cell to those that are Lineage including macrophage is 5.56 times higher in cohort 1 than in cohort 2.
+    # ...
+
+    # On average, the ratio of the number of cells that are T helper cells antigen-experienced and Naive T helper cell to those that are Lineage including monocyte is 7.75 times higher in cohort 1 than in cohort 2.
+    # ...
+
+    # On average, the ratio of the number of cells that are T regulatory cellss and T helper cell antigen-experienced to those that are Lineage including monocyte is 95.74 times higher in cohort 1 than in cohort 2.
+    # ...
+
+    # On average, the ratio of the number of cells that are T regulatory cellss and T helper cell antigen-experienced to those that are Lineage including macrophage is 87.68 times higher in cohort 1 than in cohort 2.
+    # ...
+
+    # channels = ['ICOS', 'PDL1', 'CD8A', 'CD20', 'CD45RA']
+    def p(channel):
+        return {'positive_markers':[channel], 'negative_markers':[]}
+
+    for channel, fold in zip(['ICOS', 'CD8A'], [0.791, 0.782]):
+        df = access.proximity([p(channel), p(channel)])
+        prox = f'proximity, {channel}+ and {channel}+'
+        print(f'Proximity of {channel} to {channel}')
+        values1 = df[df['cohort'] == '1'][prox]
+        values2 = df[df['cohort'] == '2'][prox]
+        compare(values1, values2, expected_fold=fold, show_pvalue=True, show_auc=True)
+
+    for channel, fold in zip(['ICOS', 'CD8A', 'PDL1', 'CD45RA'], [2.366, 2.708, 14.49, 0.475]):
+        df = access.spatial_autocorrelation(p(channel))
+        autocorr = f'spatial autocorrelation, {channel}+'
+        values1 = df[df['cohort'] == '1'][autocorr]
+        values2 = df[df['cohort'] == '2'][autocorr]
+        print(f'Spatial autocorrelation for {channel}+')
+        compare(values1, values2, expected_fold=fold, show_pvalue=True, show_auc=True)
+
+    for channel, fold in zip(['CD20', 'PDL1'], [2.467, 0.136]):
+        df = access.neighborhood_enrichment([p(channel), p(channel)])
+        ne = f'neighborhood enrichment, {channel}+ and {channel}+'
+        values1 = df[df['cohort'] == '1'][ne].dropna()
+        values2 = df[df['cohort'] == '2'][ne].dropna()
+        print(f'Neighborhood enrichment for {channel}+ and itself')
+        compare(values2, values1, expected_fold=fold, show_pvalue=True, do_log_fold=True, show_auc=True)
 
 
 if __name__=='__main__':

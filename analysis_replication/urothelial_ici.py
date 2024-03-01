bcc8212dc61ccc8956256b296d50e7f5fb561d7e
James Mathews <mail@jmath.read-books.org>
2024-03-01 11:22:35 -0500

    Spacing

--- analysis_replication/urothelial_ici.py
+++ analysis_replication/urothelial_ici.py
@@ -30,6 +30,7 @@ def test(host):
     fractions1, fractions2 = get_fractions(df, 'CD3+ LAG3+', 'CD3+', '1', '2', omit_zeros=False)
     compare(fractions2, fractions1, expected_fold=1.449, show_pvalue=True, show_auc=True)
 
+
     df = access.counts([cd8ki67, tumoral])
     fractions1, fractions2 = get_fractions(df, 'CD8+ KI67+ and intratumoral+', 'all cells', '1', '2', omit_zeros=False)
     compare(fractions2, fractions1, expected_fold=3.519, show_pvalue=True, show_auc=True)

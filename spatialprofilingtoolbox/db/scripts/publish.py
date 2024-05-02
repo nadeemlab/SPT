8e4ca0d488304c11a109a14a4af43bf68d93602e
J Mathews <mail@jmath.read-books.org>
2024-05-02 13:48:45 -0600

    Modify promotion mechanism to a whitelist

--- spatialprofilingtoolbox/db/scripts/publish.py
+++ spatialprofilingtoolbox/db/scripts/publish.py
@@ -10,16 +10,14 @@ from spatialprofilingtoolbox.db.publish_promote import PublisherPromoter
 def parse_args():
     parser = argparse.ArgumentParser(
         prog='spt db publish',
-        description='Promote a dataset from a (private) collection to the general, public '
-                    'collection.'
+        description='Promote datasets from a private collection to the general, public collection.',
     )
     add_argument(parser, 'database config')
     parser.add_argument(
-        '--study-name',
-        dest='study_name',
-        help='The fully-qualified name of the study to promote to public status (i.e. including '
-             '"collection: ...").',
-        required=True
+        '--collection',
+        dest='collection',
+        help='The token/label for the study collection.',
+        required=True,
     )
     return parser.parse_args()
 
@@ -27,9 +25,9 @@ def parse_args():
 def main():
     args = parse_args()
     database_config_file = get_and_validate_database_config(args)
-    study = args.study_name
+    collection = args.collection
     promoter = PublisherPromoter(database_config_file)
-    promoter.promote(study)
+    promoter.promote(collection)
 
 
 if __name__=='__main__':

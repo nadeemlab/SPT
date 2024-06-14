593f14ccc984c8ddfec22887eaac772a0914ba48
Jimmy Mathews <mathewj2@mskcc.org>
2024-06-14 17:58:03 -0400

    Add log recording for new worker container

--- build/build_scripts/record_docker_logs.sh
+++ build/build_scripts/record_docker_logs.sh
@@ -10,4 +10,5 @@ nowtime=$(TZ=GMT date -u +"%Y-%m-%dT%H:%M:%SZ") ; \
 unbuffer docker compose logs --since="$nowtime" -f testing-database > dlogs.db.txt &
 unbuffer docker compose logs --since="$nowtime" -f testing-api-server > dlogs.api.txt &
 unbuffer docker compose logs --since="$nowtime" -f testing-fast-counts-server > dlogs.od.txt &
+unbuffer docker compose logs --since="$nowtime" -f testing-fast-counts-server2 > dlogs.od2.txt &
 tail -f /dev/null

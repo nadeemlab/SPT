38844a121580e858438e6aabef83ab57dd3b93b1
Jimmy Mathews <mathewj2@mskcc.org>
2024-10-14 23:48:51 -0400

    Update test and changelog

--- test/ondemand/module_tests/test_fast_cache_assessor.py
+++ test/ondemand/module_tests/test_fast_cache_assessor.py
@@ -1,8 +1,11 @@
 
-from spatialprofilingtoolbox.ondemand.fast_cache_assessor import FastCacheAssessor
+from spatialprofilingtoolbox.ondemand.cache_assessment import CacheAssessment
 
 def test_assessor():
-    assessor = FastCacheAssessor(database_config_file=None)
+    assessor = CacheAssessment(database_config_file=None, study='Melanoma intralesional IL2')
+    assessor.assess_and_act()
+
+    assessor = CacheAssessment(database_config_file=None, study='Breast cancer IMC')
     assessor.assess_and_act()
 
 if __name__=='__main__':

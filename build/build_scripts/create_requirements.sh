0089b718ccde4759cb0eaba53b724ae1eecc4847
James Mathews <mail@jmath.read-books.org>
2025-02-17 11:24:38 -0500

    deprecate build scripts for requirements

--- build/build_scripts/create_requirements.sh
+++
@@ -1,7 +0,0 @@
-
-python -m venv venv 2>/dev/null 1>/dev/null
-source venv/bin/activate
-python -m pip install toml 2>/dev/null 1>/dev/null
-python build/build_scripts/create_requirements.py $1
-deactivate
-rm -rf venv/

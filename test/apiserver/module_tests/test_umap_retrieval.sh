ab328e2af3e7ab2edcc6c3ece3b7ef738a56f037
James Mathews <mail@jmath.read-books.org>
2025-02-21 12:53:36 -0500

    Fix name collision for test artifacts

--- test/apiserver/module_tests/test_umap_retrieval.sh
+++ test/apiserver/module_tests/test_umap_retrieval.sh
@@ -12,19 +12,19 @@ function test_cell_data_binary() {
     query="http://spt-apiserver-testing-apiserver:8080/cell-data-binary/?study=$study&sample=$sample"
 
     echo -e "Doing query $blue$query$reset_code ... "
-    curl -s "$query"  > _celldata.bin;
+    curl -s "$query"  > _celldata_u.bin;
     if [ "$?" -gt 0 ];
     then
         echo -e "${red}Error with apiserver query.$reset_code"
-        echo "Result saved to file: _celldata.bin"
+        echo "Result saved to file: _celldata_u.bin"
         exit 1
     fi
 
-    cat _celldata.bin | tail -c +21 | xxd -e -b -c 20 > _celldata.dump
-    rm _celldata.bin
+    cat _celldata_u.bin | tail -c +21 | xxd -e -b -c 20 > _celldata_u.dump
+    rm _celldata_u.bin
 
     lines1=$(wc -c < $filename)
-    lines2=$(wc -c < _celldata.dump)
+    lines2=$(wc -c < _celldata_u.dump)
     if [[ "$lines1" != "$lines2" ]];
     then
         status=1
@@ -35,12 +35,12 @@ function test_cell_data_binary() {
     [ $status -eq 0 ] || (echo "API query for cell data failed, unexpected byte count: $lines2 (expected $lines1)."; )
     if [ $status -eq 0 ];
     then
-        rm _celldata.dump
+        rm _celldata_u.dump
         echo -e "${green}Artifact matches well enough.$reset_code"
         echo
     else
         echo -e "${red}Some error with the diff command.$reset_code"
-        echo "Erroneous file saved to: _celldata.dump"
+        echo "Erroneous file saved to: _celldata_u.dump"
         exit 1
     fi
 }
--- test/apiserver/module_tests/test_umap_retrieval_compressed.sh
+++ test/apiserver/module_tests/test_umap_retrieval_compressed.sh
@@ -11,21 +11,21 @@ function test_cell_data_binary_compressed() {
     query="http://spt-apiserver-testing-apiserver:8080/cell-data-binary/?study=$study&sample=$sample"
 
     echo -e "Doing query $blue$query$reset_code ... "
-    curl -s "$query"  > _celldata.bin;
-    cat _celldata.bin | tail -c +21 | xxd -e -b -c 20 > _celldata.dump
-    rm _celldata.bin
+    curl -s "$query"  > _celldata_c.bin;
+    cat _celldata_c.bin | tail -c +21 | xxd -e -b -c 20 > _celldata_c.dump
+    rm _celldata_c.bin
 
     echo -e "Doing query $blue$query$reset_code with "br" header ... "
     curl -s -H "Accept-Encoding: br" "$query" > _celldata.brotli.bin;
-    cat _celldata.brotli.bin | brotli --decompress > _celldata.bin2
-    cat _celldata.bin2 | tail -c +21 | xxd -e -b -c 20 > _celldata.dump2
+    cat _celldata.brotli.bin | brotli --decompress > _celldata_c.bin2
+    cat _celldata_c.bin2 | tail -c +21 | xxd -e -b -c 20 > _celldata_c.dump2
     rm _celldata.brotli.bin
-    rm _celldata.bin2
+    rm _celldata_c.bin2
 
-    diff _celldata.dump _celldata.dump2
+    diff _celldata_c.dump _celldata_c.dump2
     status=$?
-    rm _celldata.dump
-    rm _celldata.dump2
+    rm _celldata_c.dump
+    rm _celldata_c.dump2
 
     if [ "$?" -gt 0 ];
     then

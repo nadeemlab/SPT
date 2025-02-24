0306cbac648347bb855d6324eb23db58234103d4
Jimmy Mathews <mathewj2@mskcc.org>
2025-02-23 22:57:27 -0500

    syntax

--- build/build_scripts/message_cache.sh
+++ build/build_scripts/message_cache.sh
@@ -1,4 +1,4 @@
 
 function initialize_message_cache() {
-    echo 'CREATE TABLE IF NOT EXISTS times(activity text, message text, started_time text, status_code int);' | sqlite3 buildcache.sqlite3
+    echo 'CREATE TABLE IF NOT EXISTS times(activity text, message text, started_time text, status_code int);' | sqlite3 buildcache.sqlite3;
 }

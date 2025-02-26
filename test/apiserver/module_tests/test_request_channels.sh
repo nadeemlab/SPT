cd1015f3a54c65553523d22e33c8327598463b61
James Mathews <mail@jmath.read-books.org>
2025-02-26 18:32:26 -0500

    normalize artifact name

--- test/apiserver/module_tests/test_request_channels.sh
+++ test/apiserver/module_tests/test_request_channels.sh
@@ -1,6 +1,6 @@
 query="http://spt-apiserver-testing-apiserver:8080/channels/?study=Melanoma%20intralesional%20IL2"
 
-curl -s $query | python -m json.tool | tee summary.json | tr '\n' ' ' | sed 's/ \+//g'
+curl -s $query | python -m json.tool | tee summary_rc.json | tr '\n' ' ' | sed 's/ \+//g'
 if [ "$?" -gt 0 ];
 then
     echo "Error with apiserver query."
@@ -8,16 +8,16 @@ then
     exit 1
 fi
 
-diff module_tests/expected_channels.json summary.json
+diff module_tests/expected_channels.json summary_rc.json
 status=$?
 [ $status -eq 0 ] || (echo "API query for phenotype summary failed."; )
 
 if [ $status -eq 0 ];
 then
-        rm summary.json
+        rm summary_rc.json
     exit 0
 else
-        cat summary.json
-        rm summary.json
+        cat summary_rc.json
+        rm summary_rc.json
     exit 1
 fi

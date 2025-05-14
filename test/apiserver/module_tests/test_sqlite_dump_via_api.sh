3079861d39f85151cffca758092d8309a19000a7
James Mathews <mail@jmath.read-books.org>
2025-05-14 16:51:02 -0400

    update test

--- test/apiserver/module_tests/test_sqlite_dump_via_api.sh
+++ test/apiserver/module_tests/test_sqlite_dump_via_api.sh
@@ -1,26 +1,31 @@
 
 study="Melanoma+intralesional+IL2"
 
+rm -f db1.db db2.db db3.db
+
 query="http://spt-apiserver-testing-apiserver:8080/sqlite/?study=$study"
-curl -s "$query"
+curl -s "$query" > db1.db
 status=$?
 if [ $status -gt 0 ];
 then
+    echo "Problem with query: $query" >&2
     exit $status
 fi;
 
 query="http://spt-apiserver-testing-apiserver:8080/sqlite/?study=$study&no_feature_values="
-curl -s "$query"
+curl -s "$query" > db2.db
 status=$?
 if [ $status -gt 0 ];
 then
+    echo "Problem with query: $query" >&2
     exit $status
 fi;
 
 query="http://spt-apiserver-testing-apiserver:8080/sqlite/?study=$study&no_feature_values=&no_feature_specifications="
-curl -s "$query"
+curl -s "$query" > db3.db
 status=$?
 if [ $status -gt 0 ];
 then
+    echo "Problem with query: $query" >&2
     exit $status
 fi;

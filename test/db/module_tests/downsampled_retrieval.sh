49b867e11998f355f79c32f79a7b421d90f3a2c3
James Mathews <mail@jmath.read-books.org>
2025-08-11 17:04:32 -0400

    Add snippet for inspecting new data artifact

---
+++ test/db/module_tests/downsampled_retrieval.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function get_downsampled() {
+    study="$1"
+    server="$2"
+
+    # Retrieve precomputed subsample
+    curl "http://$server/cell-data-binary-intensity-whole-study-subsample/?study=$study" --header "Accept-Encoding: br" | brotli -d - > response_decompressed.bin
+
+    # File separator byte
+    fs=$(xxd -r <<<'0 1c')
+
+    # Locate file separator
+    offset=$(grep --byte-offset -o -a "$fs" response_decompressed.bin | cut -f1 -d':')
+    echo "Offset: $offset"
+
+    # Save JSON metadata section
+    head -c $offset response_decompressed.bin  | python -m json.tool > metadata.json
+    echo "Saved metadata.json"
+
+    # Save readable representation of binary section
+    offset2=$(( offset + 2 ))
+    tail -c +$offset2 response_decompressed.bin | xxd -b -c 21 > rows.txt
+    echo "Saved rows.txt"
+}

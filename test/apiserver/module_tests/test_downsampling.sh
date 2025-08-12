cdb07c032226ca807e510216cfa577b997fa64ee
James Mathews <mail@jmath.read-books.org>
2025-08-12 13:55:20 -0400

    ignore download

---
+++ test/apiserver/module_tests/test_downsampling.sh
@@ -0,0 +1,25 @@
+
+function consider_exit() {
+    if [[ "$1" != "0" ]];
+    then
+        exit 1
+    fi
+}
+
+spt db cache-subsample 106 --database-config-file .spt_db.config.container;
+source module_tests/downsampled_retrieval.sh
+
+study='Melanoma+intralesional+IL2'
+server=spt-apiserver-testing-apiserver:8080
+
+get_downsampled "$study" "$server"
+
+diff module_tests/expected_metadata.json metadata.json
+status=$?
+if [[ "$status" != "0" ]]; then echo 'Something wrong with metadata.json.'; cat metadata.json; rm metadata.json; fi;
+consider_exit $status
+
+diff module_tests/expected_rows.txt rows.txt
+status=$?
+if [[ "$status" != "0" ]]; then echo 'Something wrong with rows.txt.'; cat rows.txt; rm rows.txt; fi;
+consider_exit $status
